0a5c23ffbe91bc2217c59780c5fea68b3c18105e
boba-and-beer <jacky2wong@gmail.com>
2021-07-11 02:02:50 +1000

    Fix the alias default

--- vecdb/client.py
+++ vecdb/client.py
@@ -16,14 +16,12 @@ class VecDBClient(HelperMixin):
         project: str=os.getenv("VDB_PROJECT"), 
         api_key: str=os.getenv("VDB_API_KEY"),
         base_url: str="https://api-dev-aueast.relevance.ai/v1/"):
-        """
-        Params:
-            local: if local is True, then the base_url switches to the default locally hosted one.
-        """
         print("Welcome to the development version of the VecDB Python SDK. Please do not use this SDK to build core systems.")
         self.project = project
         self.api_key = api_key
         self.base_url = base_url
         self.config = Config()
-        self.datasets = Datasets(project=project, api_key=api_key, base_url=base_url)
-        self.services = Services(project=project, api_key=api_key, base_url=base_url)
+        self.datasets = Datasets(project=project, 
+            api_key=api_key, base_url=base_url)
+        self.services = Services(project=project, 
+            api_key=api_key, base_url=base_url)
