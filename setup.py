9d10cd8ea43e945d0a9ae90d745fc1614b10f076
boba-and-beer <jacky2wong@gmail.com>
2021-07-13 16:13:42 +1000

    make smaller setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VecDB',
+    version='0.1.0',
+    url='',
+    author='Relevance AI',
+    author_email='dev@vctr.ai',
+    description='No description',
+    packages=find_packages(),    
+    install_requires=[],
+)
