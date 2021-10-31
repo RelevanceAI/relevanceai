85588ee84a87002223da46d3615073cb63f5d51d
boba_and_beer <jacky2wong@gmail.com>
2021-10-31 22:49:48 +1100

    Update datasets.py

--- vecdb/datasets.py
+++ vecdb/datasets.py
@@ -9,7 +9,15 @@ def get_games_dataset() -> list:
         "https://www.freetogame.com/api/games"
     ).json()
 
+def get_online_retail_dataset(number_of_documents: int=1000) -> list:
+    """Online retail dataset from UCI machine learning
+    """
+    df = pd.read_excel("https://archive.ics.uci.edu/ml/machine-learning-databases/00352/Online%20Retail.xlsx")
+    if number_of_documents is None:
+        return df.to_dict(orient='records')
+    return df.to_dict(orient='records')[:number_of_documents]
+
 def get_ecommerce_dataset():
-    """Function to download a sample ecommerce dataset
+    """Ecommerce dataset
     """
     raise NotImplementedError
