1533bce8140ff3fc3a7dacf8387de1339771658e
Charlene Leong <charleneleong84@gmail.com>
2021-11-01 04:49:39 +0000

    Skipping full tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,9 +1,10 @@
-
+import pytest
 
 def test_get_games_dataset_subset():
     from vecdb.datasets import get_games_dataset
     assert len(get_games_dataset(number_of_documents=100))==100
 
+@pytest.mark.skip(reason="Min time to insight")
 def test_get_games_dataset_full():
     from vecdb.datasets import get_games_dataset
     assert len(get_games_dataset(number_of_documents=None))==365
@@ -12,6 +13,7 @@ def test_get_online_retail_dataset_subset():
     from vecdb.datasets import get_online_retail_dataset
     assert len(get_online_retail_dataset(number_of_documents=1000))==1000
 
+@pytest.mark.skip(reason="Min time to insight")
 def test_get_online_retail_dataset_full():
     from vecdb.datasets import get_online_retail_dataset
     assert len(get_online_retail_dataset(number_of_documents=None))==406829
@@ -20,6 +22,7 @@ def test_get_get_news_dataset_subset():
     from vecdb.datasets import get_news_dataset
     assert len(get_news_dataset(number_of_documents=100))==100
 
+@pytest.mark.skip(reason="Min time to insight")
 def test_get_get_news_dataset_full():
     from vecdb.datasets import get_news_dataset
     assert len(get_news_dataset(number_of_documents=None))==250
@@ -28,6 +31,7 @@ def test_get_ecommerce_dataset_subset():
     from vecdb.datasets import get_ecommerce_dataset
     assert len(get_ecommerce_dataset(number_of_documents=1000))==1000
 
+@pytest.mark.skip(reason="Min time to insight")
 def test_get_ecommerce_dataset_full():
     from vecdb.datasets import get_ecommerce_dataset
     assert len(get_ecommerce_dataset(number_of_documents=None))==15528
