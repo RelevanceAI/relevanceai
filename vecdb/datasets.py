6c5f73743672436639ff6d8cd37f2415e001c316
chakaveh <chakaveh@daniel-remote.0v2dw5etuolerbjoxkxqdqer3b.px.internal.cloudapp.net>
2021-11-01 00:14:34 +0000

    get_ecommerce_dataset

--- vecdb/datasets.py
+++ vecdb/datasets.py
@@ -1,6 +1,8 @@
 """Datasets to mock
 """
 import requests
+import vecdb_logging
+
 
 def get_games_dataset() -> list:
     """Function to download a sample games dataset.
@@ -9,7 +11,14 @@ def get_games_dataset() -> list:
         "https://www.freetogame.com/api/games"
     ).json()
 
-def get_ecommerce_dataset():
-    """Function to download a sample ecommerce dataset
-    """
-    raise NotImplementedError
+def get_ecommerce_dataset(db_name: str = 'ecommerce-5', count: int = 1000, base_url = "https://api-aueast.relevance.ai/v1/"):
+    from .batch.client import BatchAPIClient
+
+    project = "dummy-collections"
+    api_key = "UzdYRktIY0JxNmlvb1NpOFNsenU6VGdTU0s4UjhUR0NsaDdnQTVwUkpKZw"   # read access
+    client = BatchAPIClient(project, api_key, base_url = base_url)
+    response = client.datasets.documents.list(db_name, page_size=count)
+    if "message" in response:
+        logger = vecdb_logging.create_logger()
+        logger.error(response["message"])
+    return response
