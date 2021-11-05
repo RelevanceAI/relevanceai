b0e5fc970e19eaf2a1280ba0b83ea98b00505b1b
Work Account <jacky2wong@gmail.com>
2021-11-06 07:24:37 +1100

    add module

--- vecdb/__init__.py
+++ vecdb/__init__.py
@@ -1,3 +1,8 @@
 from .http_client import VecDBClient
+# Import useful utility if possible as well
+try:
+    from jsonshower import show_json
+except ModuleNotFoundError:
+    pass
 
-__version__ = "0.12.15"
+__version__ = "0.12.16"
