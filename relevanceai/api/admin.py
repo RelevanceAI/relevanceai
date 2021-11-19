ed79c40bd8099811dd9659125c78d2bcdba2e4ae
jacky Wong <jacky2wong@gmail.com>
2021-11-19 20:16:14 +1100

    add bot

--- relevanceai/api/admin.py
+++ relevanceai/api/admin.py
@@ -9,7 +9,12 @@ class Admin(Base):
         super().__init__(project, api_key, base_url)
 
     def request_read_api_key(self, read_username: str):
-        """Creates a read only key for your project. Make sure to save the api key somewhere safe. When doing a search the admin username should still be used."""
+        """Creates a read only key for your project. Make sure to save the api key somewhere safe. When doing a search the admin username should still be used.
+
+        Args:
+            read_username:
+                Read-only project
+        """
         return self.make_http_request(
             "admin/request_read_api_key",
             method="POST",
@@ -22,10 +27,24 @@ class Admin(Base):
         source_dataset_id: str,
         source_project: str,
         source_api_key: str,
-        project: str = None,
-        api_key: str = None,
+        project: str=None,
+        api_key: str=None,
     ):
-        """Copy a dataset from another user's projects into your project. This is considered a project job
+        """Copy a dataset from another user's projects into your project.
+
+        Args:
+            dataset_id:
+                The dataset to copy
+            source_dataset_id:
+                The original dataset
+            source_project:
+                The original project to copy from
+            source_api_key:
+                The original API key of the project
+            project:
+                The original project
+            api_key:
+                The original API key
         """
         return self.make_http_request(
             "admin/copy_foreign_dataset",
