11f6aaa81277967a53df4a1e4072fab2d3e7bba0
jacky Wong <jacky2wong@gmail.com>
2021-12-01 11:49:47 +1100

    rename tests and remove aprametrize

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,11 +1,13 @@
 import pytest
 import random
 
-# TODO: Fix status_code
+# TODO: Fix status_code and identify why parametrize is failing
 #@pytest.mark.parametrize("output_format, expected_output_type", [("json", dict), ("content", bytes), ("status_code", int)])
 
-@pytest.mark.parametrize("output_format, expected_output_type", [("content", bytes)])
-def test_output_format(test_client, test_sample_dataset, output_format, expected_output_type):
+# @pytest.mark.parametrize("output_format, expected_output_type", [("content", bytes)])
+def test_output_format(test_client, test_sample_dataset):
+    output_format = "content"
+    expected_output_type = bytes
     test_client.output_format = output_format
     results = test_client.services.search.vector(
         test_sample_dataset,
@@ -18,8 +20,10 @@ def test_output_format(test_client, test_sample_dataset, output_format, expected
     )
     assert isinstance(results, expected_output_type)
 
-@pytest.mark.parametrize("output_format, expected_output_type", [("json", dict)])
-def test_output_format(test_client, test_sample_dataset, output_format, expected_output_type):
+# @pytest.mark.parametrize("output_format, expected_output_type", [("json", dict)])
+def test_output_format_json(test_client, test_sample_dataset, output_format, expected_output_type):
+    output_format = "json"
+    expected_output_type = dict
     test_client.output_format = output_format
     results = test_client.services.search.vector(
         test_sample_dataset,
