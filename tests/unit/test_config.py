22f9a8220b095896a0741852745b3af58b626c42
jacky Wong <jacky2wong@gmail.com>
2021-12-01 11:42:05 +1100

    make parametrize  independent

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,7 +4,21 @@ import random
 # TODO: Fix status_code
 #@pytest.mark.parametrize("output_format, expected_output_type", [("json", dict), ("content", bytes), ("status_code", int)])
 
-@pytest.mark.parametrize("output_format, expected_output_type", [("json", dict), ("content", bytes)])
+@pytest.mark.parametrize("output_format, expected_output_type", [("content", bytes)])
+def test_output_format(test_client, test_sample_dataset, output_format, expected_output_type):
+    test_client.output_format = output_format
+    results = test_client.services.search.vector(
+        test_sample_dataset,
+        multivector_query=[
+            {
+                "vector": [random.randint(0, 1000) for _ in range(100)],
+                "fields": ["sample_1_vector_"]
+            }
+        ]
+    )
+    assert isinstance(results, expected_output_type)
+
+@pytest.mark.parametrize("output_format, expected_output_type", [("json", dict)])
 def test_output_format(test_client, test_sample_dataset, output_format, expected_output_type):
     test_client.output_format = output_format
     results = test_client.services.search.vector(
