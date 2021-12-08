db2a72071077b46067b322e07b8d40a2f40208a8
jacky2wong <jacky2wong@gmail.com>
2021-12-08 13:55:23 +1100

    make a base class for errors

--- relevanceai/errors.py
+++ relevanceai/errors.py
@@ -1,15 +1,21 @@
 """Missing field error
 """
 
+import relevanceai
 
-class MissingFieldError(Exception):
+
+class RelevanceAIError(Exception):
+    """base class for all errors
+    """
+
+class MissingFieldError(RelevanceAIError):
     """Error handling for missing fields"""
 
 
-class APIError(Exception):
+class APIError(RelevanceAIError):
     """Error related to API"""
 
-class ClusteringResultsAlredyExistsError(Exception):
+class ClusteringResultsAlreadyExistsError(RelevanceAIError):
     """Error is raised when the clustering dataset already exists
     """
     pass
