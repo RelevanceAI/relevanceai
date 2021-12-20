a21b00990212fa3205316d5ed38ae29fc6732f9b
jacky2wong <jacky2wong@gmail.com>
2021-12-20 12:40:10 +1100

    ADd deeplink to the dashjboard for monitoring

--- relevanceai/transport.py
+++ relevanceai/transport.py
@@ -93,6 +93,19 @@ class Transport:
             self.print_dashboard_url(dashboard_url)
         return response
 
+    def _link_to_dataset_dashboard(self, dataset_id: str, suburl: str=None):
+        """Link to a monitoring dashboard
+        Suburl must be one of 
+        - "monitor"
+        - "lookups"
+        - "monitor/schema"
+        """
+        MESSAGE = "You can view your dashboard at: "
+        if suburl is None:
+            print(MESSAGE + f"https://cloud.relevance.ai/dataset/{dataset_id}/dashboard/monitor/")
+        else:
+            print(MESSAGE + f"https://cloud.relevance.ai/dataset/{dataset_id}/dashboard/{suburl}")
+
     def _log_search_to_dashboard(self, method: str, parameters: dict, endpoint: str):
         """Log search to dashboard"""
         return self._log_to_dashboard(
