109ee8253662f7599b7468c1058bf8357e0e8f1f
ChakavehSaedi <csaedi@gmail.com>
2021-12-21 15:22:55 +1100

    blackify setup

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
     "numpy>=1.19.0",
     "joblib>=1.0.0",
     "matplotlib>=3.2.2",
-    "nltk>=3.2.5"
+    "nltk>=3.2.5",
 ]
 
 excel_requirements = requirements + ["openpyxl>=3.0.9", "fsspec>=2021.10.1"]
