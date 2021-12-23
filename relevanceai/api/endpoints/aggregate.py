ce5abec91e6e22b132fb0d35936ff0b6fae731d3
felixzhu17 <80929644+felixzhu17@users.noreply.github.com>
2021-12-23 12:02:09 +1100

    black update

--- relevanceai/api/endpoints/aggregate.py
+++ relevanceai/api/endpoints/aggregate.py
@@ -110,5 +110,5 @@ class AggregateClient(_Base):
                 "flatten": flatten,
                 "alias": alias,
             },
-            base_url = "https://gateway-api-aueast.relevance.ai/v1"
+            base_url="https://gateway-api-aueast.relevance.ai/v1",
         )
