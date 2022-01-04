14591a970c328a1b0540d3eea722dde8b0db99b3
Work Account <jacky2wong@gmail.com>
2022-01-04 11:02:29 +1100

    fix versioning

--- relevanceai/__init__.py
+++ relevanceai/__init__.py
@@ -1,3 +1,4 @@
+import warnings
 from relevanceai import vector_tools
 from relevanceai.http_client import Client
 import requests
@@ -8,12 +9,17 @@ try:
 except ModuleNotFoundError:
     pass
 
-__version__ = "0.24.8"
+__version__ = "0.24.9"
 
-latest_version = requests.get("https://pypi.org/pypi/relevanceai/json").json()["info"][
-    "version"
-]
-if __version__ != latest_version:
-    print(
-        f"Your RelevanceAI version ({__version__}) is not the latest. Please install the latest version ({latest_version}) by running pip install -U relevanceai"
-    )
+try:
+    latest_version = requests.get("https://pypi.org/pypi/relevanceai/json").json()["info"][
+        "version"
+    ]
+    if __version__ != latest_version:
+        warnings.warn(
+            "Your RelevanceAI version ({version}) is not the latest. Please install the latest version ({latest_version}) by running pip install -U relevanceai".format(
+                version=__version__, latest_version=latest_version
+            )
+        )
+except:
+    pass
