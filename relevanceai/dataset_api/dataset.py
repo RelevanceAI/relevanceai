99fc34de708c1d9009ebac41e82ce8319aed468b
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-06 12:56:24 +1000

    black formatting

--- relevanceai/dataset_api/dataset.py
+++ relevanceai/dataset_api/dataset.py
@@ -6,11 +6,19 @@ import pandas as pd
 from typing import Union
 from typing import List
 
-class Dataset():
+
+class Dataset:
     def __init__(self, client) -> None:
         self.client = client
 
-    def __call__(self, dataset_id: Union[list, str], image_fields: List = [], text_fields: List = [], audio_fields: List = [], output_format: str = 'pandas'):
+    def __call__(
+        self,
+        dataset_id: Union[list, str],
+        image_fields: List = [],
+        text_fields: List = [],
+        audio_fields: List = [],
+        output_format: str = "pandas",
+    ):
         self.dataset_id = dataset_id
         self.image_fields = image_fields
         self.text_fields = text_fields
@@ -22,11 +30,14 @@ class Dataset():
     def info(self) -> None:
         health = self.client.datasets.monitor.health(self.dataset_id)
         schema = self.client.datasets.schema(self.dataset_id)
-        
-        info = {key: {'missing': health[key]['missing'], 'dtype': schema[key]} for key in health.keys()}
+
+        info = {
+            key: {"missing": health[key]["missing"], "dtype": schema[key]}
+            for key in health.keys()
+        }
         for key, value in info.items():
             print(key, value)
-    
+
     def head(self):
         # TODO
         return
