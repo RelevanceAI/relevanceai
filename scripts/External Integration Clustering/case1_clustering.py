6d3d345dd7a6016644412c7638346882592ac0f6
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-20 17:47:45 +1000

    black

--- scripts/External Integration Clustering/case1_clustering.py
+++ scripts/External Integration Clustering/case1_clustering.py
@@ -3,10 +3,10 @@ from sklearn.cluster import KMeans
 
 client = Client()
 
-df = client.Dataset('iris')
+df = client.Dataset("iris")
 
 n_clusters = 3
-vector_field = 'feature_vector_'
-alias = 'Species'
+vector_field = "feature_vector_"
+alias = "Species"
 
 X = df.cluster(vector_field, 3, overwrite=True)
--- scripts/External Integration Clustering/case2_clustering.py
+++ scripts/External Integration Clustering/case2_clustering.py
@@ -2,20 +2,22 @@ from relevanceai import Client
 from relevanceai.base import ClusterBase
 
 from sklearn.cluster import KMeans
- 
+
 client = Client()
 
-df = client.Dataset('iris')
+df = client.Dataset("iris")
 
 n_clusters = 3
-vector_field = 'feature_vector_'
+vector_field = "feature_vector_"
 
-class Clusterer(ClusterBase):  
-  def fit_transform(self, vectors):
-     return self.clusterer.fit_transform(vectors)
-  
-  def get_centroids(self):
-     return self.clusterer.cluster_centers_
+
+class Clusterer(ClusterBase):
+    def fit_transform(self, vectors):
+        return self.clusterer.fit_transform(vectors)
+
+    def get_centroids(self):
+        return self.clusterer.cluster_centers_
+
 
 kmeans = KMeans(n_clusters=n_clusters, random_state=0)
 clusterer = Clusterer(kmeans)
