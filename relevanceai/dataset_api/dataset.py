b13b307bfbeb9c749fc972565a2b7544148d1207
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-21 11:28:19 +1000

    adding set_cluster_labels

--- relevanceai/dataset_api/dataset.py
+++ relevanceai/dataset_api/dataset.py
@@ -6,6 +6,8 @@ import warnings
 import pandas as pd
 import numpy as np
 
+from doc_utils import DocUtils
+
 from typing import List, Union, Callable, Optional
 
 from relevanceai.dataset_api.groupby import Groupby, Agg
@@ -573,6 +575,19 @@ class Dataset(BatchAPIClient):
             self.dataset_id, cat_fields, updating_args={"field_name": vector_name}
         )
 
+    def set_cluster_labels(self, vector_field, alias, labels):
+        docs = self.get_all_documents(self.dataset_id)
+        docs = list(filter(DocUtils.list_doc_fields, docs))
+        vectors = self.get_field_across_documents(
+            vector_field, docs, missing_treatment="skip"
+        )
+        set_cluster_field = f"_cluster_.{vector_field}.{alias}"
+        self.set_field_across_documents(
+            set_cluster_field,
+            [f"cluster-{label}" for label in list(labels)],
+            docs,
+        )
+        return docs
 
 class Datasets(BatchAPIClient):
     """Dataset class for multiple datasets"""
--- scripts/External Integration Clustering/case2_clustering.py
+++ scripts/External Integration Clustering/case2_clustering.py
@@ -18,11 +18,23 @@ class Clusterer(ClusterBase):
     def get_centroids(self):
         return self.clusterer.cluster_centers_
 
+    def get_alias(self):
+        return type(self.clusterer).__name__
+
+    def get_labels(self):
+        return self.clusterer.labels_
+
 
 kmeans = KMeans(n_clusters=n_clusters, random_state=0)
 clusterer = Clusterer(kmeans)
 
 vectors = clusterer.fit_transform(df[vector_field].numpy())
-centroids = clusterer.get_centroids()
+df.set_cluster_labels(
+    vector_field=vector_field,
+    alias=clusterer.get_alias(),
+    labels=clusterer.get_labels(),
+)
+
+# centroids = clusterer.get_centroids()
 
 print()
