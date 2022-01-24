603f612c716e6da4c0be98f1f3aef030600792bf
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-24 15:00:32 +1000

    initial commit

---
+++ relevanceai/docutils_class/document.py
@@ -0,0 +1,5 @@
+from doc_utils.doc_utils import DocUtils
+
+class Document(DocUtils):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ relevanceai/docutils_class/document_list.py
@@ -0,0 +1,5 @@
+from doc_utils.doc_utils import DocUtils
+
+class DocumentList(DocUtils):
+    def __init__(self) -> None:
+        super().__init__()
