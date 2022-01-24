5856362877ab5951205783647f99f54fe534d1f7
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-24 15:02:00 +1000

    black

--- relevanceai/docutils_class/document.py
+++ relevanceai/docutils_class/document.py
@@ -1,5 +1,6 @@
 from doc_utils.doc_utils import DocUtils
 
+
 class Document(DocUtils):
     def __init__(self) -> None:
         super().__init__()
--- relevanceai/docutils_class/document_list.py
+++ relevanceai/docutils_class/document_list.py
@@ -1,5 +1,6 @@
 from doc_utils.doc_utils import DocUtils
 
+
 class DocumentList(DocUtils):
     def __init__(self) -> None:
         super().__init__()
