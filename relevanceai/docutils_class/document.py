f634993b996b1ac452960197e574e09c7c9c2e27
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-24 15:04:09 +1000

    added methods to both classes

--- relevanceai/docutils_class/document.py
+++ relevanceai/docutils_class/document.py
@@ -2,5 +2,13 @@ from doc_utils.doc_utils import DocUtils
 
 
 class Document(DocUtils):
-    def __init__(self) -> None:
+    def __init__(self, document) -> None:
         super().__init__()
+
+        self.document = document
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __setitem__(self, index, value):
+        raise NotImplementedError
--- relevanceai/docutils_class/document_list.py
+++ relevanceai/docutils_class/document_list.py
@@ -2,5 +2,13 @@ from doc_utils.doc_utils import DocUtils
 
 
 class DocumentList(DocUtils):
-    def __init__(self) -> None:
+    def __init__(self, documents) -> None:
         super().__init__()
+
+        self.documents = documents
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __setitem__(self, index, value):
+        raise NotImplementedError
