ce3722cf3872d9afca7ed06ec67c39192e93aa32
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-27 20:54:30 +1000

    josh

--- relevanceai/api/endpoints/datasets/cluster/centroids/centroids.py
+++ relevanceai/api/endpoints/datasets/cluster/centroids/centroids.py
@@ -87,6 +87,7 @@ class CentroidsClient(_Base):
             "include_vector": include_vector,
             "include_count": include_count,
         }
+        # JOSH CHANGE THIS PLEASE
         endpoint = "/services/cluster/centroids/list_closest_to_center"
         method = "POST"
         self._log_to_dashboard(
