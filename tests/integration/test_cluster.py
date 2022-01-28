51bdea20566bfa78a1e63d123a685f320f86277f
jacky2wong <jacky2wong@gmail.com>
2022-01-28 15:26:58 +1100

    update commit file

--- tests/integration/test_cluster.py
+++ tests/integration/test_cluster.py
@@ -88,3 +88,23 @@ def test_cluster_integration_one_liner(
         alias="sample_cluster",
     )
     assert True
+
+
+def test_dataset_api_kmeans_centroids_properties(
+    test_client: Client, test_dataset_df: Dataset
+):
+    from sklearn.cluster import KMeans
+
+    vector_field = "sample_1_vector_"
+    alias = "test_alias"
+    from relevanceai.clusterer import KMeansModel
+
+    model = KMeansModel()
+
+    clusterer: Clusterer = test_client.Clusterer(model=model, alias=alias)
+    clusterer.fit(dataset=test_dataset_df, vector_fields=[vector_field])
+
+    assert f"_cluster_.{vector_field}.{alias}" in test_dataset_df.schema
+
+    centroids = clusterer.centroids
+    assert len(centroids) > 0
