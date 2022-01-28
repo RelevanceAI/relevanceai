fe9b7190ea4bb5bee03ba556af3d18acb4cbbab9
jacky2wong <jacky2wong@gmail.com>
2022-01-28 18:17:34 +1100

    specify pandas dataframe

--- tests/unit/datasets_api/test_dataset_write.py
+++ tests/unit/datasets_api/test_dataset_write.py
@@ -31,3 +31,9 @@ def test_df_insert_csv_successful(test_csv_df: Dataset):
     """Test Insert CSv successful"""
     response, original_length = test_csv_df
     assert response["inserted"] == original_length, "incorrect insertion"
+
+
+def test_insert_df(test_dataset_df: Dataset):
+    pandas_df = pd.DataFrame({"pandas_value": [3, 2, 1], "_id": ["10", "11", "12"]})
+    test_dataset_df.insert_pandas_dataframe(pandas_df)
+    assert "pandas_value" in pandas_df.columns
