c27de83a5cdc4b59701798e1bf7703c3e3a1fab5
Work Account <jacky2wong@gmail.com>
2022-01-31 16:10:47 +1100

    update test for dr

--- tests/unit/datasets_api/test_dataset_dr.py
+++ tests/unit/datasets_api/test_dataset_dr.py
@@ -6,7 +6,10 @@ def test_reduce_dimensiosn(test_dataset_df: Dataset):
     OUTPUT_VECTOR_FIELD = "sample_1_vector_"
     ALIAS = "pca"
 
-    test_dataset_df.reduce_dimensions(vector_fields=[OUTPUT_VECTOR_FIELD], alias="pca")
+    test_dataset_df.reduce_dimensions(
+        vector_fields=[OUTPUT_VECTOR_FIELD], 
+        alias="pca"
+    )
 
     vector_field_name = ".".join(["_dr_", ALIAS, OUTPUT_VECTOR_FIELD])
     assert (
