dcc47f87f48998a3fd109cfc62d61e1a3fb9487a
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-01-31 13:32:40 +1000

    fixed typing in test_dataset_read

--- tests/unit/datasets_api/test_dataset_read.py
+++ tests/unit/datasets_api/test_dataset_read.py
@@ -11,19 +11,19 @@ def test_Dataset_init(test_client: Client, test_sample_vector_dataset):
     assert True
 
 
-def test_info(test_client, test_sample_vector_dataset):
+def test_info(test_client: Client, test_sample_vector_dataset):
     df = test_client.Dataset(test_sample_vector_dataset)
     info = df.info()
     assert True
 
 
-def test_shape(test_client, test_sample_vector_dataset):
+def test_shape(test_client: Client, test_sample_vector_dataset):
     df = test_client.Dataset(test_sample_vector_dataset)
     shape = df.shape
     assert True
 
 
-def test_head(test_client, test_sample_vector_dataset):
+def test_head(test_client: Client, test_sample_vector_dataset):
     df = test_client.Dataset(test_sample_vector_dataset)
     head = df.head()
     assert True
@@ -35,20 +35,20 @@ def test_describe(test_client: Client, test_sample_vector_dataset: Dataset):
     assert True
 
 
-def test_sample(test_client, test_sample_vector_dataset):
+def test_sample(test_client: Client, test_sample_vector_dataset):
     df = test_client.Dataset(test_sample_vector_dataset)
     sample_n = df.sample(n=10)
     assert len(sample_n) == 10
 
 
-def test_value_counts(test_client, test_sample_vector_dataset):
+def test_value_counts(test_client: Client, test_sample_vector_dataset):
     df = test_client.Dataset(test_sample_vector_dataset)
     value_counts = df["sample_1_label"].value_counts()
     value_counts = df["sample_1_label"].value_counts(normalize=True)
     assert True
 
 
-def test_filter(test_client, test_sample_vector_dataset):
+def test_filter(test_client: Client, test_sample_vector_dataset):
     df = test_client.Dataset(test_sample_vector_dataset)
     items = df.filter(items=["sample_1_label"])
     like = df.filter(like="sample_1_label")
