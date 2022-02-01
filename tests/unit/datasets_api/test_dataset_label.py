c8e007daa2f3d27b83334d5ccbb0d4e13889ba1b
jacky2wong <jacky2wong@gmail.com>
2022-02-01 20:29:03 +1100

    modularise the label tests

--- tests/unit/datasets_api/test_dataset_label.py
+++ tests/unit/datasets_api/test_dataset_label.py
@@ -1,15 +1,26 @@
-"""Testing for dataset labels 
+"""Testing for dataset labels.
 """
 import pytest
-from relevanceai.http_client import Dataset
-from ...utils import generate_random_vector
+from relevanceai.http_client import Dataset, Client
+from ...utils import generate_random_vector, generate_random_string
+from ...conftest import SAMPLE_DATASET_DATASET_PREFIX
+
+LABEL_DATSET_ID = SAMPLE_DATASET_DATASET_PREFIX + generate_random_string().lower()
 
 
-def test_dataset_labelling(test_dataset_df: Dataset):
-    results = test_dataset_df.label(
+@pytest.fixture(scope="session")
+def test_label_df(test_client: Client, sample_vector_documents):
+    df = test_client.Dataset(LABEL_DATSET_ID)
+    df.upsert_documents(sample_vector_documents)
+    yield df
+    df.delete()
+
+
+def test_dataset_labelling(test_label_df: Dataset):
+    results = test_label_df.label(
         vector_field="sample_1_vector_",
         alias="example",
-        label_dataset_id=test_dataset_df.dataset_id,
+        label_dataset_id=test_label_df.dataset_id,
         label_fields=["sample_1_label"],
         label_vector_field="sample_1_vector_",
         filters=[
@@ -27,17 +38,17 @@ def test_dataset_labelling(test_dataset_df: Dataset):
             # },
         ],
     )
-    assert "_label_.example" in test_dataset_df.schema, "schema is incorrect"
+    assert "_label_.example" in test_label_df.schema, "schema is incorrect"
     assert len(results["failed_documents"]) == 0, "failed to label documents :("
 
 
-def test_labelling_vector(test_dataset_df: Dataset):
+def test_labelling_vector(test_label_df: Dataset):
     ALIAS = "sample"
-    result = test_dataset_df.label_vector(
+    result = test_label_df.label_vector(
         generate_random_vector(100),
         label_vector_field="sample_1_vector_",
         alias=ALIAS,
-        label_dataset_id=test_dataset_df.dataset_id,
+        label_dataset_id=test_label_df.dataset_id,
         label_fields=["sample_1_label"],
         number_of_labels=1,
     )
