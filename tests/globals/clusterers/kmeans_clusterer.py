0d8c3cd9e5ff4f199551cd04a8ed461932136ab2
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-02-09 17:02:55 +1000

    fixed clusterer fixtures

--- tests/globals/clusterers/kmeans_clusterer.py
+++ tests/globals/clusterers/kmeans_clusterer.py
@@ -1,12 +1,11 @@
 import pytest
-from relevanceai.http_client import Client, ClusterOps
+
+from relevanceai.http_client import Dataset
 
 from .utils import VECTOR_FIELDS
 
 
 @pytest.fixture(scope="session")
-def kmeans_clusterer(test_client: Client, vector_dataset_id: str):
-    df = test_client.Dataset(vector_dataset_id)
-    clusterer: ClusterOps = df.auto_cluster("kmeans-20", vector_fields=VECTOR_FIELDS)
-    yield clusterer
-    clusterer.delete_centroids(df.dataset_id, VECTOR_FIELDS)
+def kmeans_clusterer(test_df: Dataset):
+    clusterer = test_df.auto_cluster("kmeans-20", vector_fields=VECTOR_FIELDS)
+    return clusterer
