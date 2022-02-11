c5d24f747e7fbcc8ae3b45f001cbb304c3576fbc
Work Account <jacky2wong@gmail.com>
2022-02-11 12:56:08 +1100

    Update to v1.0.1

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Relevance AI"
 author = "Relevance AI"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.0.1"
 
 
 # -- General configuration ---------------------------------------------------
