f34dfebdfc5911c106e50ad1e0514c8344b5375e
jacky2wong <jacky2wong@gmail.com>
2022-02-14 19:07:09 +1100

    change dataset ID to ioslate

--- tests/integration/test_dr_on_cluster.py
+++ tests/integration/test_dr_on_cluster.py
@@ -10,8 +10,9 @@ def test_dr_on_cluster(test_client: Client):
     docs = mock_documents()
     # Reduce dimensions
 
-    ds = test_client.Dataset("sample")
+    ds = test_client.Dataset("sample456")
     ds.insert_documents(docs)
     ds.auto_reduce_dimensions("pca-3", vector_fields=["sample_1_vector_"])
     ds.auto_cluster("kmeans-2", vector_fields=["_dr_.pca-3.sample_1_vector_"])
+    ds.delete()
     assert True
