e80d4525fec6bc8544526c2ee566777618468ed6
Eugene O'Friel <eugene.ofriel@relevance.ai>
2022-02-16 15:18:56 -0500

    added Dataset.to_pandas_dataframe method

--- relevanceai/dataset_api/dataset_export.py
+++ relevanceai/dataset_api/dataset_export.py
@@ -9,6 +9,30 @@ from relevanceai.analytics_funcs import track
 
 
 class Export(Read):
+    def to_pandas_dataframe(self, **kwargs) -> pd.DataFrame:
+        """
+        Converts a Relevance AI Dataset to a pandas DataFrame.
+
+        Parameters
+        ----------
+        kwargs: Optional
+            see client.get_all_documents() for extra args
+
+        Example
+        -------
+        .. code-block::
+            from relevanceai import Client
+
+            client = Client()
+
+            relevanceai_dataset = client.Dataset("dataset_id")
+            df = relevance_ai.to_pandas_dataframe()
+        """
+        documents = self.get_all_documents(**kwargs)
+        df = pd.DataFrame(documents)
+        df.set_index("_id", inplace=True)
+        return df
+
     @track
     def to_csv(self, filename: str, **kwargs):
         """
