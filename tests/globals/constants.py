0a7ab854a7dba50bfdd5a5a85d77b9521b8157b1
Work Account <jacky2wong@gmail.com>
2022-02-20 20:01:06 +1100

    Fix number of docs

--- tests/globals/constants.py
+++ tests/globals/constants.py
@@ -5,7 +5,7 @@ import string
 from typing import List
 
 VECTOR_LENGTH = 8
-NUMBER_OF_DOCUMENTS = os.getenv("TEST_NUMBER_OF_DOCUMENTS", 20)
+NUMBER_OF_DOCUMENTS = int(os.getenv("TEST_NUMBER_OF_DOCUMENTS", 20))
 
 
 def generate_random_string(string_length: int = 5) -> str:
