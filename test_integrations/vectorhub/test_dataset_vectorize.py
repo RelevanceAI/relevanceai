b76c788d2eea413a7c9c85fd1f68fe5dee0ad075
Eugene O'Friel <eugene.ofriel@relevance.ai>
2022-02-23 11:03:55 -0500

    modify test

--- test_integrations/vectorhub/test_dataset_vectorize.py
+++ test_integrations/vectorhub/test_dataset_vectorize.py
@@ -4,6 +4,5 @@ from relevanceai.http_client import Dataset
 def test_dataset_vectorize(test_dataset: Dataset):
     results = test_dataset.vectorize(image_fields=["image_url"], text_fields=["data"])
     # This means the results are good yay!
-    assert results is None
     assert "image_url_clip_vector_" in test_dataset.schema
     assert "data_use_vector_" in test_dataset.schema
