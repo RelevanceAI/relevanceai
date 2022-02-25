3368e99556577b712e101b2dd794b23dd87c92ef
jacky2wong <jacky2wong@gmail.com>
2022-02-25 14:36:12 +1100

    add documentation to setting fields

--- relevanceai/dim_reduction_ops/dim_reduction_ops.py
+++ relevanceai/dim_reduction_ops/dim_reduction_ops.py
@@ -27,12 +27,37 @@ from doc_utils import DocUtils
 
 class ReduceDimensionsOps(DocUtils):
     def __init__(self, model, alias: str):
+        """
+        Dim Reduction Ops
+
+        Parameters
+        --------------
+
+        model
+            The model to run dimensionality reduction. This requires a `fit_transform` method.
+        alias: str
+            The alias of the dimensionality reduction
+        """
         self.model = model
+        if not hasattr(model, "fit_transform"):
+            raise AttributeError("‼️ Model needs to have a fit_transform method.")
         self.alias = alias
 
     def reduce_dimensions(self, fields: list, documents: list, inplace: bool = True):
         """
         Reduce Dimensions
+
+        Parameters
+        --------------
+
+        fields: list
+            The list of fields to run dimensionality reduction on. Currently
+            only supports 1 field.
+        documents: list
+            The list of documents to run dimensionality reduction on
+        inplace: bool
+            If True, replaces the original documents, otherwise it returns
+            a new set of documents with only the dr vectors in it and the _id
         """
         self.fields = fields
         self.documents = documents
@@ -41,16 +66,36 @@ class ReduceDimensionsOps(DocUtils):
         else:
             raise ValueError("Supporting multiple fields not supported yet.")
         dr_values = self.model.fit_transform(values)
-        if inplace:
-            self.set_dr_field_across_documents(fields, dr_values, documents)
-            return documents
-        else:
-            new_documents = copy.deepcopy(documents)
-            self.set_dr_field_across_documents(fields, dr_values, new_documents)
-            return new_documents
+        new_documents = self.set_dr_field_across_documents(
+            fields, dr_values, documents, inplace=inplace
+        )
+        return new_documents
 
-    def set_dr_field_across_documents(self, fields: list, values, documents):
+    def set_dr_field_across_documents(
+        self, fields: list, values: list, documents: list, inplace: bool = True
+    ):
+        """
+        Setting the DR field allows users to quickly follow Relevance AI
+        best practice in naming.
+        It follows `_dr_.*field_name*.alias`
+
+        Parameters
+        -------------
+
+        fields:
+            The list of fields to set
+        values:
+            The list of values to set
+        documents:
+            List of documents to set
+
+        """
         fields_joined = ".".join(fields)
         field_name = f"_dr_.{fields_joined}.{self.alias}"
-        self.set_field_across_documents(field_name, values, documents)
-        return documents
+
+        if inplace:
+            self.set_field_across_documents(field_name, values, documents)
+            return documents
+        new_documents = [{"_id": d["_id"]} for d in documents]
+        self.set_field_across_documents(field_name, values, new_documents)
+        return new_documents
