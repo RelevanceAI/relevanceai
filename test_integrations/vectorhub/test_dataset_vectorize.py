9ad7d671ba1f62308ad8a3cf35491478843bb4c8
ofrighil <32347630+ofrighil@users.noreply.github.com>
2022-02-28 17:54:38 -0500

    feature/pro-1447-change-output-of-vectorize (#505)
    Update `Dataset.vectorize` to ignore already-vectorized fields and modified output to include those vector names

--- test_integrations/vectorhub/test_dataset_vectorize.py
+++ test_integrations/vectorhub/test_dataset_vectorize.py
@@ -6,3 +6,6 @@ def test_dataset_vectorize(test_dataset: Dataset):
     # This means the results are good yay!
     assert "image_url_clip_vector_" in test_dataset.schema
     assert "data_use_vector_" in test_dataset.schema
+
+    results = test_dataset.vectorize(image_fields=["image_url"])
+    assert "image_url_clip_vector_" in results["skipped_vectors"]
