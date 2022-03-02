ec9167f49b055bd916e9b839dc95718c33cc71e9
Work Account <jacky2wong@gmail.com>
2022-03-02 23:32:51 +1100

    move conftest for tests

--- test_integrations/hdbscan/test_hdbscan.py
+++ test_integrations/hdbscan/test_hdbscan.py
@@ -1,11 +1,11 @@
 from relevanceai import Client
 
 
-def test_hdbscan(test_df):
+def test_hdbscan(test_client, test_df):
     import hdbscan
 
     model = hdbscan.HDBSCAN()
-    clusterer = client.ClusterOps(alias="hdbscan", model=model)
+    clusterer = test_client.ClusterOps(alias="hdbscan", model=model)
     clusterer.fit_predict_update(test_df, vector_fields=["sample_1_vector_"])
     docs = clusterer.get_centroid_documents()
     all_ids = [d["_id"] for d in docs]
