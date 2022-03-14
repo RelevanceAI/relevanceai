b5e0a02f41afc7c7d23145d0d2344746f083c182
Work Account <jacky2wong@gmail.com>
2022-03-14 21:09:37 +1100

    fix tuple to list

--- relevanceai/package_utils/list_to_tuple.py
+++ relevanceai/package_utils/list_to_tuple.py
@@ -5,7 +5,13 @@ def list_to_tuple(function):
     @wraps(function)
     def wrapper(*args, **kw):
         args = [tuple(x) if type(x) == list else x for x in args]
-        result = function(*args, **kw)
+        new_kw = {}
+        for k, v in kw.items():
+            if type(v) == list:
+                new_kw[k] = tuple(v)
+            else:
+                new_kw[k] = v
+        result = function(*args, **new_kw)
         result = tuple(result) if type(result) == list else result
         return result
 
