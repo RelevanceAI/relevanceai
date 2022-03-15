b22150fd7e483245b6e739c9ec5d938c31f69c15
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-03-15 10:15:13 +1000

    testing

--- relevanceai/dataset/vis/plot.py
+++ relevanceai/dataset/vis/plot.py
@@ -7,14 +7,18 @@ class Plot(Statistics):
     def plot(
         self,
         dr_alias: str,
-        number_of_documents: Union[None, int],
+        number_of_documents: Union[None, int] = None,
         show_progress_bar: bool = True,
     ):
+        schema = self.schema
+        if dr_alias not in schema:
+            raise ValueError("You must reduce your vectors before you can plot")
+
         import pandas as pd
 
-        pd.options.plotting.backend = "ploty"
+        pd.options.plotting.backend = "plotly"
 
-        print("Retrieving all documents")
+        print("Retrieving documents")
         if number_of_documents is None:
             documents = self.get_all_documents(
                 select_fields=[dr_alias], show_progress_bar=show_progress_bar
@@ -23,8 +27,10 @@ class Plot(Statistics):
             documents = self.get_documents(
                 number_of_documents=number_of_documents,
                 select_fields=[dr_alias],
-                show_progress_bar=show_progress_bar,
             )
 
         dr_algo = dr_alias.split(".")
         df = pd.DataFrame(documents)
+
+        fig = df.plot()
+        fig.show()
