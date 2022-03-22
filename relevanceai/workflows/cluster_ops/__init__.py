1d70288538ae93e04240cefcfefdfd678a1e64bb
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-03-22 16:33:35 +1000

    fixed imports

--- relevanceai/workflows/cluster_ops/__init__.py
+++ relevanceai/workflows/cluster_ops/__init__.py
@@ -1,9 +1,3 @@
 """ClusterOps
 """
-# from relevanceai.ops.clusterops.clusterops import ClusterOps
-# from relevanceai.ops.clusterops.clusterbase import (
-#     ClusterBase,
-#     CentroidClusterBase,
-#     AdvancedCentroidClusterBase,
-# )
-# from relevanceai.ops.clusterops.kmeans_clusterer import KMeansModel
+from .ops import ClusterOps
