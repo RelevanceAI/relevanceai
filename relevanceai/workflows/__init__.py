143b162e3d1ac85dc72c16d1e6d2d77afc90f2f7
jacky2wong <jacky2wong@gmail.com>
2022-03-22 14:07:06 +1100

    move workflows

--- relevanceai/workflows/__init__.py
+++ relevanceai/workflows/__init__.py
@@ -1,1 +1,6 @@
 from relevanceai.workflows.base import Workflow
+from relevanceai.workflows.cluster_ops.clusterops import ClusterOps
+from relevanceai.workflows.sentiment_ops.sentiment_workflow import SentimentOps
+from relevanceai.workflows.dim_reduction_ops.dim_reduction_ops import (
+    ReduceDimensionsOps,
+)
