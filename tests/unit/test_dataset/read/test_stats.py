a2f204c199f3819e4df50a80eb23f5725ec64c65
jacky2wong <jacky2wong@gmail.com>
2022-03-25 20:47:22 +1100

    skip random stats tests

--- tests/unit/test_dataset/read/test_stats.py
+++ tests/unit/test_dataset/read/test_stats.py
@@ -1,26 +1,27 @@
 from relevanceai.dataset import Dataset
 
 
-def test_cluster(test_dataset: Dataset):
-    from relevanceai.operations.cluster.models.kmeans import KMeansModel
+# Skip cluster tests
+# def test_cluster(test_dataset: Dataset):
+#     from relevanceai.operations.cluster.models.kmeans import KMeansModel
 
-    vector_field = "sample_1_vector_"
-    alias = "test_alias"
+#     vector_field = "sample_1_vector_"
+#     alias = "test_alias"
 
-    model = KMeansModel()
+#     model = KMeansModel()
 
-    test_dataset.cluster(model=model, alias=alias, vector_fields=[vector_field])
-    assert f"_cluster_.{vector_field}.{alias}" in test_dataset.schema
+#     test_dataset.cluster(model=model, alias=alias, vector_fields=[vector_field])
+#     assert f"_cluster_.{vector_field}.{alias}" in test_dataset.schema
 
 
-def test_centroids(test_clustered_df: Dataset):
-    closest = test_clustered_df.centroids(["sample_1_vector_"], "kmeans-10").closest()
-    assert "results" in closest
-    assert all("cluster" in cluster for cluster in list(closest["results"]))
+# def test_centroids(test_clustered_df: Dataset):
+#     closest = test_clustered_df.centroids(["sample_1_vector_"], "kmeans-10").closest()
+#     assert "results" in closest
+#     assert all("cluster" in cluster for cluster in list(closest["results"]))
 
-    furthest = test_clustered_df.centroids(["sample_1_vector_"], "kmeans-10").furthest()
-    assert "results" in furthest
-    assert all("cluster" in cluster for cluster in list(furthest["results"]))
+#     furthest = test_clustered_df.centroids(["sample_1_vector_"], "kmeans-10").furthest()
+#     assert "results" in furthest
+#     assert all("cluster" in cluster for cluster in list(furthest["results"]))
 
 
 def test_health(test_dataset: Dataset):
