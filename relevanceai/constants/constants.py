c34a0fbfc1d0fc47928cb140d1cb86fea8ec3bf3
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-03-27 18:14:58 +1000

    added news to datasets list

--- relevanceai/constants/constants.py
+++ relevanceai/constants/constants.py
@@ -15,7 +15,7 @@ TRANSIT_ENV_VAR = "_IS_ANALYTICS_IN_TRANSIT"
 GLOBAL_DATASETS = ["_mock_dataset_"]
 
 DATASETS = [
-    "games",
+    "news" "games",
     "ecommerce_1",
     "ecommerce_2",
     "ecommerce_3",
