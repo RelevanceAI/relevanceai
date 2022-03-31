d713d01596f8b18c789a0b28fe40bc98c4a0c069
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-03-31 15:28:41 +1000

    fixed tests

--- tests/unit/test_dataset/test_dataset.py
+++ tests/unit/test_dataset/test_dataset.py
@@ -139,7 +139,7 @@ class TestDatasetStats:
         describe = test_dataset.describe()
         assert isinstance(describe, pd.DataFrame)
 
-    @pytest.mark.skip(msg="requires matplotlib not sure hot handle")
+    @pytest.mark.skip(msg="requires matplotlib not sure how to handle")
     def test_corr(self, test_dataset: Dataset):
         test_dataset.cluster(model="kmeans", vector_fields=["sample_1_vector_"])
         corr = test_dataset.corr(
