587f65b5d03901d73d7d263f1202c7d59bdecf8e
Jacky Wong <jacky2wong@gmail.com>
2022-04-01 18:59:50 +1100

    update ops

--- relevanceai/operations/base.py
+++ relevanceai/operations/base.py
@@ -15,15 +15,18 @@ class BaseOps:
         for key, value in kwargs:
             self.__setattr__(key, value)
 
-    def from_credentials(self, credentials: Credentials):
+    @staticmethod
+    def from_credentials(credentials: Credentials):
         kwargs = dict(credentials=credentials)
         self.init(**kwargs)
 
-    def from_token(self, token):
+    @staticmethod
+    def from_token(token):
         kwargs = dict(token=token)
         self.init(**kwargs)
 
-    def from_details(self, project: str, api_key: str, region: str):
+    @staticmethod
+    def from_details(project: str, api_key: str, region: str):
         kwargs = dict(
             project=project,
             api_key=api_key,
@@ -31,7 +34,8 @@ class BaseOps:
         )
         self.init(**kwargs)
 
-    def from_dataset(self, dataset: Any, alias: str, vector_fields: List[str]):
+    @staticmethod
+    def from_dataset(dataset: Any, alias: str, vector_fields: List[str]):
         if isinstance(dataset, str):
             dataset_id = dataset
         else:
@@ -42,4 +46,4 @@ class BaseOps:
             alias=alias,
             vector_fields=vector_fields,
         )
-        self.init(**kwargs)
+        # self.init(**kwargs)
