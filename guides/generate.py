d37897416e136b1ccf407d810001c06e6ef79943
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-04-04 11:12:50 +1000

    base guides folder and generate scripts.py

---
+++ guides/generate.py
@@ -0,0 +1,11 @@
+"""
+A script for generating .rst files for /docsrc from .ipynb files in this folder
+"""
+
+
+def main():
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
