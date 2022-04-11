dcfc78620dc9dbdb2cc10dad5cfea2083c34801b
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-04-11 15:34:40 +1000

    merged test_search.py and test_vectorize.py

--- tests/unit/test_operations/test_vectorize.py
+++ tests/unit/test_operations/test_vectorize.py
@@ -1,13 +1,30 @@
 import random
 
 from relevanceai import Client
+from relevanceai.dataset import Dataset
 from relevanceai.utils.datasets import (
     get_iris_dataset,
     get_online_ecommerce_dataset,
     get_palmer_penguins_dataset,
 )
 
-from tests.globals.constants import SAMPLE_DATASET_DATASET_PREFIX
+
+from tests.globals.constants import (
+    SAMPLE_DATASET_DATASET_PREFIX,
+    generate_random_label,
+    generate_random_string,
+    generate_random_vector,
+    generate_random_integer,
+)
+
+
+MULTIVECTOR_QUERY = [
+    {"vector": generate_random_vector(), "fields": ["sample_1_vector_"]}
+]
+
+CHUNK_MULTIVECTOR_QUERY = [
+    {"vector": generate_random_vector(), "fields": ["_chunk_.label_chunkvector_"]}
+]
 
 
 class TestVectorizeOps:
@@ -77,3 +94,32 @@ class TestVectorizeOps:
             "Stage_customtextencoder_vector_",
         ]
         assert all(vector in dataset.schema for vector in vectors)
+
+    def test_vector_search(self, test_dataset: Dataset):
+        test_dataset.vector_search(
+            multivector_query=MULTIVECTOR_QUERY,
+        )
+        assert True
+
+    def test_hybrid_search(self, test_dataset: Dataset):
+        test_dataset.hybrid_search(
+            multivector_query=MULTIVECTOR_QUERY,
+            text="hey",
+            fields=["sample_1_label"],
+        )
+        assert True
+
+    def test_chunk_search(self, test_dataset: Dataset):
+        test_dataset.chunk_search(
+            multivector_query=CHUNK_MULTIVECTOR_QUERY,
+            chunk_field="_chunk_",
+        )
+        assert True
+
+    def test_multistep_chunk_search(self, test_dataset: Dataset):
+        test_dataset.multistep_chunk_search(
+            multivector_query=CHUNK_MULTIVECTOR_QUERY,
+            first_step_multivector_query=MULTIVECTOR_QUERY,
+            chunk_field="_chunk_",
+        )
+        assert True
