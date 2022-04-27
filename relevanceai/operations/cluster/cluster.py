3526f7bce557c6b999e5c67f730b2cff3b27a748
Jacky Wong <jacky2wong@gmail.com>
2022-04-27 12:43:34 +1000

    ignore useless typehint

--- relevanceai/operations/cluster/cluster.py
+++ relevanceai/operations/cluster/cluster.py
@@ -126,7 +126,7 @@ class ClusterOps(ClusterUtils, BaseOps, DocUtils):
             self.package = self._get_package(self.model)
 
         self.alias = self._get_alias(alias)
-        self.vector_fields = vector_fields
+        self.vector_fields = vector_fields # type: ignore
         self.outlier_value = outlier_value
         self.outlier_label = outlier_label
 
