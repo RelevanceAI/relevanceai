08f107456f938a0c72ab830149409384b90aca43
Joseph Twin <joseph.twin@relevance.ai>
2022-05-04 15:39:43 +1000

    set local variable as well

--- relevanceai/operations/cluster/cluster.py
+++ relevanceai/operations/cluster/cluster.py
@@ -106,6 +106,7 @@ class ClusterOps(ClusterUtils, BaseOps, DocUtils):
         self.n_clusters = n_clusters
         if "n_clusters" in self.cluster_config:
             self.n_clusters = self.cluster_config["n_clusters"]
+            n_clusters = self.cluster_config["n_clusters"]
 
         if n_clusters is not None:
             self.cluster_config["n_clusters"] = n_clusters  # type: ignore
