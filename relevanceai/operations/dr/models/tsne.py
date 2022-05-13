151fc8bd14d91173c6bb732a604303e5b9bb50b5
Jacky Wong <jacky2wong@gmail.com>
2022-05-13 16:12:34 +1000

    remove pca from tsne

--- relevanceai/operations/dr/models/tsne.py
+++ relevanceai/operations/dr/models/tsne.py
@@ -14,11 +14,8 @@ class TSNE(DimReductionBase):
         dr_args: Optional[Dict[Any, Any]] = DIM_REDUCTION_DEFAULT_ARGS["tsne"],
         dims: int = 3,
     ) -> np.ndarray:
-        from sklearn.decomposition import PCA
         from sklearn.manifold import TSNE
 
-        pca = PCA(n_components=min(10, vectors.shape[1]))
-        data_pca = pca.fit_transform(vectors)
         self.logger.debug(f"{dr_args}")
         tsne = TSNE(n_components=dims, **dr_args)
-        return tsne.fit_transform(data_pca)
+        return tsne.fit_transform(vectors)
