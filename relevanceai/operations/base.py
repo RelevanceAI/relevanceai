cb5c2ceb423685fc650c0516a034d2d7156619dd
Jacky Koh <jacky.koh@vylar.org>
2022-05-15 00:57:18 +1000

    add vector operations to base.py

--- relevanceai/operations/base.py
+++ relevanceai/operations/base.py
@@ -1,6 +1,7 @@
 """
 Base class for operations.
 """
+import numpy as np
 from typing import Any, List, Union
 from relevanceai.client.helpers import (
     Credentials,
@@ -96,3 +97,24 @@ class BaseOps:
             return new_alias
         else:
             return alias if alias.endswith("_vector_") else alias + vector_suffix
+
+    #vector related operations, maybe new file?
+    def _vector_operation(self, vectors, vector_operation: str = "mean", axis=0):
+        """
+        Args:
+            Vectors: the list of vectors to include
+            vector_operation: One of ['mean', 'minus', 'sum', 'min', 'max']
+            axis: The axis to which to perform the operation
+        """
+        if vector_operation == "mean":
+            return np.mean(vectors, axis=axis).tolist()
+        elif vector_operation == "minus":
+            return np.subtract(vectors, axis=axis).tolist()
+        elif vector_operation == "sum":
+            return np.sum(vectors, axis=axis).tolist()
+        elif vector_operation == "min":
+            return np.min(vectors, axis=axis).tolist()
+        elif vector_operation == "max":
+            return np.max(vectors, axis=axis).tolist()
+        else:
+            return np.mean(vectors, axis=axis).tolist()
