fb2fd61a142bdc4af0d474f5c36466997088fd22
Joseph Twin <joseph.twin@relevance.ai>
2022-05-16 16:36:15 +1000

    what I have so far

--- relevanceai/operations_new/vectorize/base.py
+++ relevanceai/operations_new/vectorize/base.py
@@ -1,25 +1,120 @@
-from typing import Any, List, Union
+from typing import Dict, Iterable, List, Any, Union
 
 from relevanceai.operations_new.base import OperationsBase
 from relevanceai.operations_new.vectorize.models.base import ModelBase
 
 
-class VectorizeOps(OperationsBase):
-    tfhub_models: List[str] = []
-    sentencetransformer_models: List[str] = []
+class VectorizeBase(OperationsBase):
 
-    def __init__(self, fields: List[str], models: List[Any]):
+    models: List[ModelBase]
+    fields: List[str]
 
-        self.fields = fields
-        self.models = [self._get_model(model) for model in models]
+    TFHUB_MODELS: Dict[str, Dict[str, int]] = {
+        "https://tfhub.dev/google/universal-sentence-encoder/4": {
+            "vector_length": 512,
+        },
+    }
+    SENTENCE_TRANSFORMER_MODELS: Dict[str, Dict[str, int]] = {
+        "distilroberta-base-paraphrase-v1": {
+            "vector_length": 768,
+        },
+        "xlm-r-distilroberta-base-paraphrase-v1": {
+            "vector_length": 768,
+        },
+        "paraphrase-xlm-r-multilingual-v1": {
+            "vector_length": 768,
+        },
+        "distilbert-base-nli-stsb-mean-tokens": {
+            "vector_length": 768,
+        },
+        "bert-large-nli-stsb-mean-tokens": {
+            "vector_length": 1024,
+        },
+        "roberta-base-nli-stsb-mean-tokens": {
+            "vector_length": 768,
+        },
+        "roberta-large-nli-stsb-mean-tokens": {
+            "vector_length": 1024,
+        },
+        "distilbert-base-nli-stsb-quora-ranking": {
+            "vector_length": 768,
+        },
+        "distilbert-multilingual-nli-stsb-quora-ranking": {
+            "vector_length": 768,
+        },
+        "distilroberta-base-msmarco-v1": {
+            "vector_length": 768,
+        },
+        "distiluse-base-multilingual-cased-v2": {
+            "vector_length": 512,
+        },
+        "xlm-r-bert-base-nli-stsb-mean-tokens": {
+            "vector_length": 768,
+        },
+        "bert-base-wikipedia-sections-mean-tokens": {
+            "vector_length": 768,
+        },
+        "LaBSE": {
+            "vector_length": 768,
+        },
+        "average_word_embeddings_glove.6B.300d": {
+            "vector_length": 300,
+        },
+        "average_word_embeddings_komninos": {
+            "vector_length": 300,
+        },
+        "average_word_embeddings_levy_dependency": {
+            "vector_length": 768,
+        },
+        "average_word_embeddings_glove.840B.300d": {
+            "vector_length": 300,
+        },
+        "paraphrase-xlm-r-multilingual-v1": {
+            "vector_length": 768,
+        },
+    }
 
-    def _get_model(self, model: Any) -> Union[None, ModelBase]:
+    def _get_model(self, model: Any) -> ModelBase:
+        """If the model is a string, then it is either a TFHUB model or a Sentence Transformer model. If it
+        is a TFHUB model, then return None. If it is a Sentence Transformer model, then return a
+        SentenceTransformer2Vec object
+
+        Parameters
+        ----------
+        model : Any
+            str = "bert-base-nli-mean-tokens"
+
+        Returns
+        -------
+            The model is being returned.
+
+        """
         if isinstance(model, str):
-            if model in self.tfhub_models:
-                return None
+            if model in self.TFHUB_MODELS:
+                from relevanceai.operations_new.vectorize.models.text.tfhub import (
+                    TFHubText2Vec,
+                )
 
-            elif model in self.sentencetransformer_models:
-                return None
+                vector_length = self.TFHUB_MODELS[model]
+                model = TFHubText2Vec(
+                    url=model,
+                    vector_length=vector_length,
+                )
+
+                return model
+
+            elif model in self.SENTENCE_TRANSFORMER_MODELS:
+                from relevanceai.operations_new.vectorize.models.text.sentence_transformers import (
+                    SentenceTransformer2Vec,
+                )
+
+                vector_length = self.SENTENCE_TRANSFORMER_MODELS[model]
+                model = SentenceTransformer2Vec(
+                    model=model,
+                    vector_length=vector_length,
+                )
+
+                return model
 
             else:
                 raise ValueError("Model not a valid model string")
@@ -32,7 +127,20 @@ class VectorizeOps(OperationsBase):
                 "Model should either be a supported model string or inherit from ModelBase"
             )
 
-    def run(self, documents):
+    def run(self, documents: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        """It takes a list of documents, and for each document, it runs the document through each of the
+        models in the pipeline, and returns the updated documents.
+
+        Parameters
+        ----------
+        documents : List[Dict[str, Any]]
+            List[Dict[str, Any]]
+
+        Returns
+        -------
+            A list of dictionaries.
+
+        """
 
         updated_documents = documents
 
---
+++ relevanceai/operations_new/vectorize/models/text/sentence_transformers/model.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from relevanceai.operations_new.vectorize.models.base import ModelBase
+from relevanceai.utils.decorators.vectors import catch_errors
+
+
+class SentenceTransformer2Vec(ModelBase):
+    def __init__(self, model, vector_length):
+        from sentence_transformers import SentenceTransformer
+
+        self.model: SentenceTransformer = model
+        self.vector_length: int = vector_length
+
+    @catch_errors
+    def encode(self, text: str) -> List[float]:
+        """The function takes a string as input and returns a list of floats
+
+        Parameters
+        ----------
+        text : str
+            The text to be encoded.
+
+        Returns
+        -------
+            A list of floats.
+
+        """
+        return self.model.encode([text])[0].tolist()
+
+    @catch_errors
+    def bulk_encode(self, texts: List[str]) -> List[List[float]]:
+        """The function takes a list of strings and returns a list of lists of floats
+
+        Parameters
+        ----------
+        texts : List[str]
+            List[str]
+
+        Returns
+        -------
+            A list of lists of floats.
+
+        """
+        return self.model.encode(texts).tolist()
