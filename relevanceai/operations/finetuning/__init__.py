5ec8114644ab18f74cc34fe00d98a2ccd2799e95
Joseph Twin <joseph.twin@relevance.ai>
2022-05-16 10:21:35 +1000

    fixed imports

---
+++ relevanceai/operations/finetuning/__init__.py
@@ -0,0 +1,4 @@
+from relevanceai.operations.text.finetuning.supervised_finetuning_ops import (
+    SupervisedTripleLossFinetuneOps,
+)
+from relevanceai.operations.text.finetuning.unsupervised_finetuning_ops import GPLOps
