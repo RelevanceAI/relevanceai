6c8362f7bce3294b54eb393d7e09a5bd44190566
Joseph Twin <joseph.twin@relevance.ai>
2022-05-19 11:05:10 +1000

    fixed test:

--- tests/unit/test_operations/test_dr.py
+++ tests/unit/test_operations/test_dr.py
@@ -4,11 +4,11 @@ from relevanceai.dataset import Dataset
 def test_reduce_dimensions(test_dataset: Dataset):
     model = "pca"
     alias = "pca"
-    n_components = 3
+    dims = 3
     vector_field = "sample_1_vector_"
     test_dataset.reduce_dims(
         model=model,
-        n_components=n_components,
+        dims=dims,
         vector_fields=[vector_field],
         alias=alias,
     )
