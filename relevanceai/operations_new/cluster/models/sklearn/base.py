8ab9c37210034b017fb6b5beb1282a0df6c9a14f
Jacky Wong <jacky2wong@gmail.com>
2022-05-22 21:42:17 +1000

    add sklearn default

--- relevanceai/operations_new/cluster/models/sklearn/base.py
+++ relevanceai/operations_new/cluster/models/sklearn/base.py
@@ -1,11 +1,17 @@
 """
 Sklearn Base
 """
-
 from relevanceai.operations_new.cluster.models.base import ModelBase
+from relevanceai.utils.doc_utils.doc_utils import DocumentList
+from typing import List, Union
+from sklearn.base import ClusterMixin
 
 
-class SklearnModelBase(ModelBase):
+class SklearnModelBase(ModelBase, ClusterMixin):
     """Sklearn model base"""
 
-    pass
+    def fit_predict(self, *args, **kwargs) -> List:
+        return super().fit_predict(*args, **kwargs)
+
+    def predict_documents(self, documents: Union[List, DocumentList]) -> DocumentList:
+        pass
