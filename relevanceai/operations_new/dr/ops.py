75ad146fc29f0c998995da204239bba87abfc2ba
Jacky Wong <jacky2wong@gmail.com>
2022-05-22 17:32:38 +1000

    fix init

--- relevanceai/operations_new/dr/ops.py
+++ relevanceai/operations_new/dr/ops.py
@@ -14,13 +14,16 @@ class DimReductionOps(DimReductionBase, OperationsAPIBase):
         model,
         n_components: int,
         model_kwargs: Optional[dict] = None,
-        *args,
         **kwargs
     ):
         if model_kwargs is None:
             model_kwargs = {}
         super().__init__(
-            model=model, n_components=n_components, model_kwargs=model_kwargs, **kwargs
+            model=model,
+            n_components=n_components,
+            model_kwargs=model_kwargs,
+            alias=alias,
+            **kwargs
         )
         self.model = self._get_model(
             model=model, n_components=n_components, alias=alias, **model_kwargs
