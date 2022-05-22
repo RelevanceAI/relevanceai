9621c8f93a79162532cc96804165aabe78e58036
Jacky Wong <jacky2wong@gmail.com>
2022-05-22 17:10:23 +1000

    fix kwargs in init

--- relevanceai/operations_new/dr/ops.py
+++ relevanceai/operations_new/dr/ops.py
@@ -9,4 +9,5 @@ class DimReductionOps(DimReductionBase, OperationsAPIBase):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        super(OperationsAPIBase).__init__(*args, **kwargs)
+        for k, v in kwargs.items():
+            setattr(self, k, v)
