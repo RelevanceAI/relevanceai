9fe4ceab2cfa0cd0f4a22d30a8e4e769f117f367
Jacky Wong <jacky2wong@gmail.com>
2022-05-22 17:28:29 +1000

    fix model kwargs

--- relevanceai/operations_new/dr/ops.py
+++ relevanceai/operations_new/dr/ops.py
@@ -1,5 +1,6 @@
 from relevanceai.operations_new.apibase import OperationsAPIBase
 from relevanceai.operations_new.dr.base import DimReductionBase
+from typing import Optional
 
 
 class DimReductionOps(DimReductionBase, OperationsAPIBase):
@@ -7,8 +8,22 @@ class DimReductionOps(DimReductionBase, OperationsAPIBase):
     API related Functionality for Operation
     """
 
-    def __init__(self, model, *args, **kwargs):
-        super().__init__(model=model, *args, **kwargs)
-        self.model = self._get_model(model)
+    def __init__(
+        self,
+        alias,
+        model,
+        n_components: int,
+        model_kwargs: Optional[dict] = None,
+        *args,
+        **kwargs
+    ):
+        if model_kwargs is None:
+            model_kwargs = {}
+        super().__init__(
+            model=model, n_components=n_components, model_kwargs=model_kwargs, **kwargs
+        )
+        self.model = self._get_model(
+            model=model, n_components=n_components, alias=alias, **model_kwargs
+        )
         for k, v in kwargs.items():
             setattr(self, k, v)
