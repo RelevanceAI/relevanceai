3b84a52ff1b566a7b7176fcfdd774caac5bbb2f0
Jacky Wong <jacky2wong@gmail.com>
2022-05-23 23:01:01 +1000

    set up base for odcs

---
+++ relevanceai/operations_new/cluster/partial/base.py
@@ -0,0 +1,11 @@
+from relevanceai.operations_new.apibase import OperationAPIBase
+
+
+class PartialClusterModelBase:
+    def partial_fit(self, documents):
+        # Run partial fitting on documents
+        pass
+
+    def transform(self, documents):
+        # Transform the documents
+        pass
