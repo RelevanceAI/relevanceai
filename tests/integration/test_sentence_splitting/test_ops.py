9ba7846fb8b0c4da4798048cc835fdd9bb7b314e
Ardhan Madras <ardhan@rocksis.net>
2022-05-23 12:04:08 +0800

    Added SentenceSplitterOps tests

---
+++ tests/integration/test_sentence_splitting/test_ops.py
@@ -0,0 +1,35 @@
+from relevanceai import Client
+
+from relevanceai.operations_new.processing.text.sentence_splitting.ops import (
+    SentenceSplitterOps,
+)
+
+
+class TestSentenceSplitterOps:
+    def test_split_text(self, test_client: Client):
+        ops = SentenceSplitterOps(credentials=test_client.credentials)
+        text = """Loudermilk and GOP Rep. Rodney Davis, the top Republican on the House Administration Committee,
+        issued a joint statement later Thursday responding to the committee's letter again pushing back on
+        any allegation of "reconnaissance" tours on January 5 and calling for Capitol Police to release
+        the footage."""
+        texts = ops.split_text(
+            text=text,
+            language="en",
+        )
+        for t in texts:
+            assert t in text
+
+    def test_split_text_document(self, test_client: Client):
+        ops = SentenceSplitterOps(credentials=test_client.credentials)
+        document = {
+            "status": 0,
+            "params": {
+                "facet_range": "initial_release_date",
+                "facet_limit": "300",
+                "facet_range_gap": "+1YEAR",
+            },
+        }
+        documents = ops.split_text_document(
+            text_field="params.facet_limit", document=document, output_field="output"
+        )
+        assert documents["output"][0] == {"params.facet_limit": "300"}
