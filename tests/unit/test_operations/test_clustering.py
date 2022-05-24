bc701164738f231a6d9f10db1c26a1fdec143e33
Jacky Wong <jacky2wong@gmail.com>
2022-05-24 16:48:17 +1000

    fix merge test

--- tests/unit/test_operations/test_clustering.py
+++ tests/unit/test_operations/test_clustering.py
@@ -132,7 +132,7 @@ class TestClusterOps:
             vector_fields=["sample_1_vector_"],
         )
 
-        ops.merge(cluster_labels=["cluster-0", "cluster-1"], alias="new_clustering")
+        ops.merge(cluster_labels=["cluster_0", "cluster_1"], alias="new_clustering")
 
         centroids = test_client.datasets.cluster.centroids.list(
             dataset_id=test_dataset.dataset_id,
