d0c393733da1cbc174d36f820522ff2396721d5a
Jacky Wong <jacky2wong@gmail.com>
2022-05-24 13:40:55 +1000

    Ad test for bach clustering

---
+++ tests/unit/test_operations_new/test_cluster/test_batch/test_batch_cluster.py
@@ -0,0 +1,25 @@
+from relevanceai import Client
+from relevanceai.dataset import Dataset
+
+
+def test_batch_cluster(test_sample_dataset: Dataset):
+
+    test_sample_dataset.batch_cluster(
+        vector_fields=["sample_1_vector_"],
+        model="minibatchkmeans",
+        model_kwargs={"n_clusters": 15},
+    )
+    alias = "minibatchkmeans-15"
+    assert alias in test_sample_dataset.schema
+
+
+def test_batch_cluster_kmeans_integration(test_sample_dataset: Dataset):
+    from sklearn.cluster import MiniBatchKMeans
+
+    model = MiniBatchKMeans(n_clusters=20)
+    test_sample_dataset.batch_cluster(
+        vector_fields=["sample_1_vector_"],
+        model=model,
+    )
+    alias = "minibatchkmeans-20"
+    assert alias in test_sample_dataset.schema
