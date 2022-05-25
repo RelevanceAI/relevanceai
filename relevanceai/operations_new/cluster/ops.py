70e9d5566722a7465ae520f74bf8517920af5c29
Joseph Twin <joseph.twin@relevance.ai>
2022-05-25 11:11:38 +1000

    found bug

--- relevanceai/operations_new/cluster/ops.py
+++ relevanceai/operations_new/cluster/ops.py
@@ -512,56 +512,6 @@ class ClusterOps(ClusterBase, OperationAPIBase, ClusterAlias):
             )
         raise ValueError("Algorithm needs to be either `relational` or `centroid`.")
 
-    def _get_n_clusters(self):
-        if "n_clusters" in self.model_kwargs:
-            return self.model_kwargs["n_clusters"]
-        elif hasattr(self.model, "n_clusters"):
-            return self.model.n_clusters
-        elif hasattr(self.model, "k"):
-            return self.model.k
-        return None
-
-    def _generate_alias(self) -> str:
-        # Issue a warning about auto-generated alias
-        # We auto-generate certain aliases if the model
-        # is a default model like kmeans or community detection
-        n_clusters = self._get_n_clusters()
-        if hasattr(self.model, "alias"):
-            return self.model.alias
-
-        if n_clusters is not None:
-            alias = f"{self.model_name}-{n_clusters}"
-        else:
-            alias = f"{self.model_name}"
-        if self.verbose:
-            print(f"The alias is `{alias.lower()}`.")
-
-        Warning.MISSING_ALIAS.format(alias=alias)
-        return alias
-
-    def _get_alias(self, alias: Any) -> str:
-        # Depending a package
-        # Get the alias
-        self._get_package_from_model(self.model)
-        if self.package == "sklearn":
-            self.alias = self._get_alias_from_sklearn()
-            if self.alias is not None:
-                return self.alias
-
-        if alias is not None and isinstance(alias, str):
-            return alias
-
-        alias = self._generate_alias()
-        return alias.lower()
-
-    def _get_alias_from_sklearn(self):
-        if hasattr(self.model, "n_clusters"):
-            return f"{self.model_name}-{self.model.n_clusters}"
-        elif hasattr(self.model, "k"):
-            return f"{self.model_name}-{self.model.k}"
-        else:
-            return f"{self.model_name}"
-
     def store_operation_metadatas(self):
         self.store_operation_metadata(
             operation="cluster",
