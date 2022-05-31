08a63c786b0c4505524bc2ff6390bea6ab7c98c9
Jacky Wong <jacky2wong@gmail.com>
2022-05-31 16:27:05 +1000

    add max_features parameter

--- relevanceai/operations_new/vectorize/models/text/sklearn/tfidf.py
+++ relevanceai/operations_new/vectorize/models/text/sklearn/tfidf.py
@@ -1,15 +1,27 @@
 """A simple tfidf implementation
 """
+from typing import Optional
 from relevanceai.operations_new.vectorize.models.base import VectorizeModelBase
 
 
 class TfIDFModel(VectorizeModelBase):
     # Simple sklearn tfidf vect
-    def __init__(self, text_field: str, output_field: str = None):
+    def __init__(
+        self,
+        text_field: str,
+        output_field: str = None,
+        model_kwargs: Optional[dict] = None,
+    ):
         # TODO: refactor this
         from sklearn.feature_extraction.text import TfidfVectorizer
 
-        self.vect = TfidfVectorizer()
+        if model_kwargs is None:
+            model_kwargs = {}
+
+        if "max_features" not in model_kwargs:
+            model_kwargs["max_features"] = 2048
+
+        self.vect = TfidfVectorizer(**model_kwargs)
         self.text_field = text_field
         self.output_field = (
             output_field if output_field is not None else self._get_output_field()
