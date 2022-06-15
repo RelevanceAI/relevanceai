affd87c93cc20254806badb2e33d83453ed99391
Jacky Wong <jacky2wong@gmail.com>
2022-06-15 10:17:19 +1000

    add test cleaning

--- relevanceai/operations_new/processing/text/clean/ops.py
+++ relevanceai/operations_new/processing/text/clean/ops.py
@@ -6,10 +6,12 @@ from relevanceai.operations_new.processing.text.clean.base import (
     CleanTextBase,
 )
 
+
 class CleanTextOps(CleanTextBase, OperationAPIBase):
     """
     Clean text operations
     """
+
     @property
     def name(self):
         return "clean-text"
