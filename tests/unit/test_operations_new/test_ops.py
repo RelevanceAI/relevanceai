9b9290bf6592bb1fb28d8cb406b4fc4ce6fa968d
Jacky Wong <jacky2wong@gmail.com>
2022-06-16 11:03:34 +1000

    add a new test

---
+++ tests/unit/test_operations_new/test_ops.py
@@ -0,0 +1,19 @@
+"""
+Test that analyzing text works out of the box
+"""
+import pytest
+from relevanceai.dataset import Dataset
+
+
+@pytest.mark.skip(reason="Slow to run")
+def test_analyze_text(test_dataset: Dataset):
+    test_dataset.analyze_text(
+        fields=["sample_1_label"],
+        output_fields=["sample_vector_"],
+        vectorize=True,
+        cluster=True,
+        subcluster=True,
+        extract_sentiment=True,
+        extract_emotion=True,
+    )
+    assert "sample_1_vector_" in test_dataset.schema
