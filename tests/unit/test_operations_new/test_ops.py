e9748e6281549e49dad58f13b900de60412973aa
Jacky Wong <jacky2wong@gmail.com>
2022-06-16 13:58:57 +1000

    update ops

--- tests/unit/test_operations_new/test_ops.py
+++ tests/unit/test_operations_new/test_ops.py
@@ -9,11 +9,11 @@ from relevanceai.dataset import Dataset
 def test_analyze_text(test_dataset: Dataset):
     test_dataset.analyze_text(
         fields=["sample_1_label"],
-        output_fields=["sample_vector_"],
+        vector_fields=["sample_vector_"],
         vectorize=True,
         cluster=True,
         subcluster=True,
         extract_sentiment=True,
         extract_emotion=True,
     )
-    assert "sample_1_vector_" in test_dataset.schema
+    assert "sample_vector_" in test_dataset.schema
