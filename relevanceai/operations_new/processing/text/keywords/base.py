a6168e87ea6b4d792bb23d98f3ed26d2d923529f
Jacky Wong <jacky2wong@gmail.com>
2022-06-18 19:01:25 +1000

    Add keyphrase operation

---
+++ relevanceai/operations_new/processing/text/keywords/base.py
@@ -0,0 +1,63 @@
+from relevanceai.operations_new.base import OperationBase
+
+
+class KeyWordBase(OperationBase):
+    """
+    Extract keyphrase from documents
+    """
+
+    def __init__(
+        self,
+        fields: list,
+        model_name: str = "all-mpnet-base-v2",
+        lower_bound: int = 0,
+        upper_bound: int = 3,
+        output_fields: list = None,
+        stop_words: list = None,
+    ):
+        self.fields = fields
+        self.model_name = model_name
+        self.output_fields = output_fields
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.stop_words = stop_words
+
+    def _get_output_field(self, field):
+        return field + "_keyphrase_"
+
+    @property
+    def name(self):
+        return "keyphrase"
+
+    @property
+    def keyphrase_model(self):
+        if not hasattr(self, "_model"):
+            from keybert import KeyBERT
+
+            self._model = KeyBERT(self.model_name)
+        return self._model
+
+    def extract_keyphrase(self, text):
+        keywords = self.keyphrase_model.extract_keywords(
+            text,
+            keyphrase_ngram_range=(self.lower_bound, self.upper_bound),
+            stop_words=self.stop_words,
+        )
+        return keywords
+
+    def transform(self, documents):
+        # Extract the keywords from a bunch of documents
+        keyphrase_docs = [{"_id": d["_id"]} for d in documents]
+        for i, t in enumerate(self.fields):
+            if self.output_fields is not None:
+                output_field = self.output_fields[i]
+            else:
+                output_field = self._get_output_field(t)
+            keyphrases = [
+                self.extract_keyphrase(
+                    self.get_field(t, doc, missing_treatment="return_empty_string"),
+                )
+                for doc in documents
+            ]
+            self.set_field_across_documents(output_field, keyphrases, keyphrase_docs)
+        return keyphrase_docs
