241504232843f4ce42e503007c1743b4a99cefe4
jacky koh <jacky@relevance.ai>
2022-07-14 15:08:59 +1000

    change default list arguments

--- relevanceai/apps/explorer_app/app.py
+++ relevanceai/apps/explorer_app/app.py
@@ -1,87 +1,113 @@
 from typing import Dict, List, Optional, Union
 from relevanceai.apps.explorer_app.base import ExplorerBase
 
-class ExplorerApp(ExplorerBase):
-    def preview_fields(self, fields=[], append=True):
-        self._append_or_replace("default-preview-centroids-columns", fields, default_value=[])
 
-    #Search and filter section
-    def facets(self, fields=[], append=True):
+class ExplorerApp(ExplorerBase):
+    def preview_fields(self, fields: List = None, append=True):
+        fields = [] if fields is None else fields
+        self._append_or_replace(
+            "default-preview-centroids-columns", fields, default_value=[]
+        )
+
+    # Search and filter section
+    def facets(self, fields: List = None, append=True):
+        fields = [] if fields is None else fields
         self._append_or_replace("filtering-facets", fields, default_value=[])
 
-    def sort(self, metrics=[], append=True):
+    def sort(self, metrics: List = None, append=True):
+        metrics = [] if metrics is None else metrics
         if "key-metrics" not in self.config:
-            self.config["key-metrics"]["previewQuery"] = {"groupby" : [], "metrics" : []}
+            self.config["key-metrics"]["previewQuery"] = {"groupby": [], "metrics": []}
         if append:
-            self.config["key-metrics"]["previewQuery"]["metrics"] += self.dataset._clean_metrics(metrics, name_prefix="")[0]
+            self.config["key-metrics"]["previewQuery"][
+                "metrics"
+            ] += self.dataset._clean_metrics(metrics, name_prefix="")[0]
         else:
-            self.config["key-metrics"]["previewQuery"]["metrics"] = self.dataset._clean_metrics(metrics, name_prefix="")[0]
+            self.config["key-metrics"]["previewQuery"][
+                "metrics"
+            ] = self.dataset._clean_metrics(metrics, name_prefix="")[0]
 
-    def sort_default(self, metric_name):
+    def sort_default(self, metric_name: str):
         if "key-metrics" not in self.config or not self.config["key-metrics"]:
             raise TypeError("'sort' needs to be specified to use 'sort_default'")
         if isinstance(metric_name, str):
-            self.config['sort-default'] = self.dataset._return_sort_in_metrics(
-                metric_name, 
-                self.config["key-metrics"]["previewQuery"]["metrics"]
+            self.config["sort-default"] = self.dataset._return_sort_in_metrics(
+                metric_name, self.config["key-metrics"]["previewQuery"]["metrics"]
             )
         else:
             raise TypeError("'sort_default' needs to be of type string.")
 
-    def search_fields(self, fields=[]):
+    def search_fields(self, fields: List = None):
+        fields = [] if fields is None else fields
         self.config["text-fields-to-search"] = fields
-    
-    def vector_search_fields(self, vector_fields=[]):
+
+    def vector_search_fields(self, vector_fields: List = None):
+        vector_fields = [] if vector_fields is None else vector_fields
         self.config["vector-fields-to-search"] = vector_fields
-    
-    def search_min_relevance(self, min_relevance):
+
+    def search_min_relevance(self, min_relevance: int):
         self.config["search-minimum-relevance"] = min_relevance
-    
-    #Chart section
+
+    # Chart section
     def chart(
-        self, 
-        groupby:List[Dict]=[], 
-        metrics:List[Dict]=[], 
-        sort:List[Dict]=None, 
-        page_size:int=None,
-        chart_name:str=None, 
-        chart_mode:str="column", 
-        show_frequency:bool=False,
+        self,
+        groupby: List[Dict] = None,
+        metrics: List[Dict] = None,
+        sort: List[Dict] = None,
+        page_size: int = None,
+        chart_name: str = None,
+        chart_mode: str = "column",
+        show_frequency: bool = False,
         **kwargs
     ):
+        groupby = [] if groupby is None else groupby
+        metrics = [] if metrics is None else metrics
+
         main_groupby = self.dataset._clean_groupby(groupby)[0]
         main_metrics = self.dataset._clean_metrics(metrics)[0]
         chart_config = self._agg_to_chart_config(
-            main_groupby, 
+            main_groupby,
             main_metrics,
             sort=sort,
-            page_size=page_size, 
+            page_size=page_size,
             chart_name=chart_name,
-            chart_mode=chart_mode, 
-            show_frequency=show_frequency, 
+            chart_mode=chart_mode,
+            show_frequency=show_frequency,
             **kwargs
         )
-        self._append_or_replace("aggregation-charts", [self._agg_to_chart_config(**chart_config)], default_value=[])
+        self._append_or_replace(
+            "aggregation-charts",
+            [self._agg_to_chart_config(**chart_config)],
+            default_value=[],
+        )
 
     def charts(self, charts, append=True):
         if isinstance(charts, dict):
-            self._append_or_replace("aggregation-charts", [self._agg_to_chart_config(**charts)], default_value=[])
+            self._append_or_replace(
+                "aggregation-charts",
+                [self._agg_to_chart_config(**charts)],
+                default_value=[],
+            )
         elif isinstance(charts, list):
-            self._append_or_replace("aggregation-charts", [self._agg_to_chart_config(**c) for c in charts], default_value=[])
+            self._append_or_replace(
+                "aggregation-charts",
+                [self._agg_to_chart_config(**c) for c in charts],
+                default_value=[],
+            )
         else:
             raise TypeError("'charts' needs to be a list or dictionary.")
 
-    #Cluster section
-    def cluster(self, cluster_field):
+    # Cluster section
+    def cluster(self, cluster_field: str):
         self.config["cluster"] = cluster_field
 
-    def cluster_charts(self, charts):
+    def cluster_charts(self, charts: List):
         self._append_or_replace("cluster-preview-config", charts, default_value=[])
 
-    def label_clusters(self, cluster_labels):
+    def label_clusters(self, cluster_labels: Dict):
         if "cluster" in self.config:
-            alias = self.config["cluster"].split('.')[-1]
-            vector_fields = ".".join(self.config["cluster"].split('.')[1:-1])
+            alias = self.config["cluster"].split(".")[-1]
+            vector_fields = ".".join(self.config["cluster"].split(".")[1:-1])
             self.dataset.label_clusters(cluster_labels, alias, vector_fields)
         else:
             raise Exception("Specify your cluster field first with .cluster.")
--- relevanceai/apps/explorer_app/base.py
+++ relevanceai/apps/explorer_app/base.py
@@ -1,16 +1,17 @@
 import warnings
 from typing import Dict, List, Optional, Union
 
+
 class ExplorerBase:
-    def __init__(self, 
-        name, 
-        dataset, 
-        deployable_id=None,
-        default_view="charts", 
-        charts_view_column=2,
-        preview_centroids_page_size=2,
-        client=None,
-        **kwargs
+    def __init__(
+        self,
+        name: str,
+        dataset,
+        deployable_id: str = None,
+        default_view: str = "charts",
+        charts_view_column: int = 2,
+        preview_centroids_page_size: int = 2,
+        **kwargs,
     ):
         self.name = name
         # if not isinstance(dataset, Dataset):
@@ -19,7 +20,9 @@ class ExplorerBase:
         self.dataset_id = dataset.dataset_id
         self.deployable_id = deployable_id
         if default_view not in ["charts", "documents", "categories"]:
-            raise KeyError("'default_view' can only be one of ['charts', 'documents', 'categories']")
+            raise KeyError(
+                "'default_view' can only be one of ['charts', 'documents', 'categories']"
+            )
         self.default_view = default_view
         app_config = None
         self.reloaded = False
@@ -28,67 +31,80 @@ class ExplorerBase:
                 app_config = self.dataset.get_app(deployable_id)
                 self.reloaded = True
             except:
-               raise Exception(f"{deployable_id} does not exist in the dataset, the given id will be used for creating a new app.")            
+                raise Exception(
+                    f"{deployable_id} does not exist in the dataset, the given id will be used for creating a new app."
+                )
         if app_config:
             self.config = app_config["configuration"]
         else:
             self.config = {
-                "dataset_name" : self.dataset_id,
-                "deployable_name" : name,
-                "type":"explore", 
-                "charts-view-columns" : charts_view_column,
-                "results-mode" : default_view,
-                "preview-centroids-page-size" : preview_centroids_page_size,
-                **kwargs
+                "dataset_name": self.dataset_id,
+                "deployable_name": name,
+                "type": "explore",
+                "charts-view-columns": charts_view_column,
+                "results-mode": default_view,
+                "preview-centroids-page-size": preview_centroids_page_size,
+                **kwargs,
             }
 
-    def deploy(self, overwrite=False):
+    def deploy(self, overwrite: bool = False):
         if self.deployable_id and self.reloaded:
-            status = self.dataset.update_app(self.deployable_id, self.config, overwrite=overwrite)
+            status = self.dataset.update_app(
+                self.deployable_id, self.config, overwrite=overwrite
+            )
             if status["status"] == "success":
                 return self.dataset.get_app(self.deployable_id)
             else:
                 raise Exception("Failed to update app")
         return self.dataset.create_app(self.config)
-        
-    def _append_or_replace(self, key, value, append=True, default_value=[]):
+
+    def _append_or_replace(
+        self, key: str, value, append: bool = True, default_value=None
+    ):
+        default_value = [] if default_value is None else default_value
+
         if key not in self.config:
-            self.config[key] = []
+            self.config[key] = default_value
         if append:
             self.config[key] += value
         else:
             self.config[key] = value
 
     def _agg_to_chart_config(
-        self, 
-        groupby:List[Dict]=[], 
-        metrics:List[Dict]=[], 
-        sort:List[Dict]=None, 
-        page_size:int=None,
-        chart_name:str=None, 
-        chart_mode:str="column", 
-        show_frequency:bool=False,
-        **kwargs
-    ):  
+        self,
+        groupby: List[Dict] = None,
+        metrics: List[Dict] = None,
+        sort: List[Dict] = None,
+        page_size=None,
+        chart_name: str = None,
+        chart_mode: str = "column",
+        show_frequency: bool = False,
+        **kwargs,
+    ):
+        groupby = [] if groupby is None else groupby
+        metrics = [] if metrics is None else metrics
+
         chart_config = {
-            "groupby" : groupby,
-            "metrics" : metrics,
-            "chart-mode" : chart_mode,
-            "chart-name" : chart_name,
-            "show-frequency" : show_frequency,
-            **kwargs
+            "groupby": groupby,
+            "metrics": metrics,
+            "chart-mode": chart_mode,
+            "chart-name": chart_name,
+            "show-frequency": show_frequency,
+            **kwargs,
         }
         if not chart_name:
-            chart_name = "Chart " # can give more flexibility to this
+            chart_name = "Chart "  # can give more flexibility to this
             if groupby:
-                groupby_names = [g['name'] for g in groupby]
+                groupby_names = [g["name"] for g in groupby]
                 chart_name += f"grouped by ({', '.join(groupby_names)}) "
             if metrics:
-                metrics_names = [m['name'] for m in metrics]
+                metrics_names = [m["name"] for m in metrics]
                 chart_name += f"with metrics ({', '.join(metrics_names)}) "
             chart_config["chart-name"] = chart_name
         if sort:
-            chart_config["metric-to-sort-by"] = self.dataset._return_sort_in_metrics(sort, metrics)
+            chart_config["metric-to-sort-by"] = self.dataset._return_sort_in_metrics(
+                sort, metrics
+            )
         if page_size:
             chart_config["page-size"] = page_size
         return chart_config
