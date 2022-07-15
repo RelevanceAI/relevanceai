a4ca3411baff510135dfda11065a8674d44af024
jacky koh <jacky@relevance.ai>
2022-07-15 13:12:24 +1000

    fix mypy errors

--- relevanceai/apps/report_app/blocks.py
+++ relevanceai/apps/report_app/blocks.py
@@ -1,16 +1,18 @@
 import io
+import uuid
 import requests
 from relevanceai.apps.report_app.marks import ReportMarks
+
 
 class ReportBlocks(ReportMarks):
     def _process_content(self, content):
         if isinstance(content, str):
-            return [{"type":"text", "text":content}]
+            return [{"type": "text", "text": content}]
         elif isinstance(content, list):
             content_list = []
             for c in content:
                 if isinstance(c, str):
-                    content_list.append({"type":"text", "text":c})
+                    content_list.append({"type": "text", "text": c})
                 elif isinstance(c, list):
                     content_list.append(c[0])
                 else:
@@ -21,74 +23,79 @@ class ReportBlocks(ReportMarks):
 
     def h1(self, content, add=True):
         block = {
-            "type" : "appBlock",
+            "type": "appBlock",
             # "attrs" : {"id": str(uuid.uuid4())},
-            "content" : [{
-                "type": "heading", 
-                "attrs" : {"level" : 1},
-                "content" : self._process_content(content)
-            }]
+            "content": [
+                {
+                    "type": "heading",
+                    "attrs": {"level": 1},
+                    "content": self._process_content(content),
+                }
+            ],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
-    
+
     def h2(self, content, add=True):
         block = {
-            "type" : "appBlock",
+            "type": "appBlock",
             # "attrs" : {"id": str(uuid.uuid4())},
-            "content" : [{
-                "type": "heading", 
-                "attrs" : {"level" : 2},
-                "content" : self._process_content(content)
-            }]
+            "content": [
+                {
+                    "type": "heading",
+                    "attrs": {"level": 2},
+                    "content": self._process_content(content),
+                }
+            ],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
 
     def h3(self, content, add=True):
         block = {
-            "type" : "appBlock",
+            "type": "appBlock",
             # "attrs" : {"id": str(uuid.uuid4())},
-            "content" : [{
-                "type": "heading", 
-                "attrs" : {"level" : 3},
-                "content" : self._process_content(content)
-            }]
+            "content": [
+                {
+                    "type": "heading",
+                    "attrs": {"level": 3},
+                    "content": self._process_content(content),
+                }
+            ],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
 
     def quote(self, content, add=True):
         block = {
-            "type" : "appBlock",
+            "type": "appBlock",
             # "attrs" : {"id": str(uuid.uuid4())},
-            "content" : [{
-                "type": "blockquote", 
-                "content" : self._process_content(content)
-            }]
+            "content": [
+                {"type": "blockquote", "content": self._process_content(content)}
+            ],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
 
     def paragraph(self, content, add=True, raw=False):
+        block = {"type": "paragraph", "content": self._process_content(content)}
+        if raw:
+            return block
         block = {
-            "type": "paragraph", 
-            "content" : self._process_content(content)
-        }
-        if raw: return block
-        block = {
-            "type" : "appBlock",
+            "type": "appBlock",
             # "attrs" : {"id": str(uuid.uuid4())},
-            "content" : [block]
+            "content": [block],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
 
     def _list_item(self, content):
-        return [{
-            "type" : "listItem",
-            "content" : [self.paragraph(content, raw=True)]
-        }]
+        return [{"type": "listItem", "content": [self.paragraph(content, raw=True)]}]
 
     def bullet_list(self, contents, add=True):
         if not isinstance(contents, list):
@@ -97,15 +104,12 @@ class ReportBlocks(ReportMarks):
         for c in contents:
             list_contents += self._list_item(c)
         block = {
-            "type" : "appBlock",
-            "content" : [{
-                "type": "bulletList", 
-                "content" : list_contents
-            }]
+            "type": "appBlock",
+            "content": [{"type": "bulletList", "content": list_contents}],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
-
 
     def ordered_list(self, contents, add=True):
         if not isinstance(contents, list):
@@ -114,41 +118,48 @@ class ReportBlocks(ReportMarks):
         for c in contents:
             list_contents += self._list_item(c)
         block = {
-            "type" : "appBlock",
-            "content" : [{
-                "type": "orderedList", 
-                "content" : list_contents
-            }]
+            "type": "appBlock",
+            "content": [{"type": "orderedList", "content": list_contents}],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
 
-    def image(self, content, title:str="", width:int=100, add:bool=True):
+    def image(
+        self, content, title: str = "", width_percentage: int = 100, add: bool = True
+    ):
         if isinstance(content, str):
             if "http" in content and "/":
-                #online image
-                content_bytes = io.BytesIO(response = requests.get(content).content).getvalue()
+                # online image
+                content_bytes = io.BytesIO(requests.get(content).content).getvalue()
             else:
-                #local filepath
-                content_bytes = io.BytesIO(open(content, 'rb').read()).getvalue()
+                # local filepath
+                content_bytes = io.BytesIO(open(content, "rb").read()).getvalue()
         elif isinstance(content, bytes):
             content_bytes = content
         elif isinstance(content, io.BytesIO):
             content_bytes = content.getvalue()
+        else:
+            raise TypeError("'content' needs to be of type str, bytes or io.BytesIO.")
+        filename = f"{title}.png" if title else f"{str(uuid.uuid4())}.png"
+        image_url = self.dataset.insert_media_bytes(
+            content_bytes, filename=filename, verbose=False
+        )
         block = {
-            "type" : "appBlock",
+            "type": "appBlock",
             # "attrs" : {"id": str(uuid.uuid4())},
-            "content" : [{
-                "type": "imageDisplayBlock", 
-                "content" : [
-                    {
-                        "imageSrc" : image_url,
-                        "title" : title,
-                        "width" : '{width}%',
-                        "height" : "auto"
-                    }
-                ]
-            }]
+            "content": [
+                {
+                    "type": "imageDisplayBlock",
+                    "attrs": {
+                        "imageSrc": image_url,
+                        "title": title,
+                        "width": f"{width_percentage}%",
+                        "height": "auto",
+                    },
+                }
+            ],
         }
-        if add: self.contents.append(block)
+        if add:
+            self.contents.append(block)
         return block
--- relevanceai/apps/report_app/sections/aggregate.py
+++ relevanceai/apps/report_app/sections/aggregate.py
@@ -1,23 +1,43 @@
 import itertools
 from typing import Dict, List, Optional, Union
-from relevanceai.apps.report_app.blocks import ReportBlocks
+from relevanceai.apps.report_app.advanced_blocks import ReportAdvancedBlocks
 
-class AggregateSections(ReportBlocks):
-    def section_overview(self, metrics:List, entity_name:str="documents", filters:List=None, decimals:int=3):
+
+class AggregateSections(ReportAdvancedBlocks):
+    def section_overview(
+        self,
+        metrics: List,
+        entity_name: str = "documents",
+        filters: List = None,
+        decimals: int = 3,
+    ):
         filters = [] if filters is None else filters
 
         main_metrics, metric_fields, _ = self.dataset._clean_metrics(metrics)
-        self.quote(f"Analyzing {self.dataset.shape[0]} {entity_name} across {self.dataset.shape[1]} fields/columns")
+        self.quote(
+            f"Analyzing {self.dataset.shape[0]} {entity_name} across {self.dataset.shape[1]} fields/columns"
+        )
         self.h1("Section: Overview")
-        overall_metrics = self.dataset.aggregate(metrics=main_metrics, filters=filters)['results'][0]
+        overall_metrics = self.dataset.aggregate(metrics=main_metrics, filters=filters)[
+            "results"
+        ][0]
         overall_metrics_bullets = []
         for k, v in overall_metrics.items():
             if k == "frequency":
                 continue
-            overall_metrics_bullets += [[self.bold(k), ": ", self.bold(str(round(v, decimals)))]]
+            overall_metrics_bullets += [
+                [self.bold(k), ": ", self.bold(str(round(v, decimals)))]
+            ]
         self.bullet_list(overall_metrics_bullets)
 
-    def section_groupby(self, groupby:List, metrics:List=None, groupby_depths:List=None, filters:List=None, decimals:int=3):
+    def section_groupby(
+        self,
+        groupby: List,
+        metrics: List = None,
+        groupby_depths: List = None,
+        filters: List = None,
+        decimals: int = 3,
+    ):
         metrics = [] if metrics is None else metrics
         groupby_depths = [1] if groupby_depths is None else groupby_depths
         filters = [] if filters is None else filters
@@ -30,37 +50,57 @@ class AggregateSections(ReportBlocks):
                 for group in main_groupby:
                     self.h2(f"Looking at ({group['name']})")
                     for order in ["desc", "asc"]:
-                        freq_group = self.dataset.aggregate(groupby=[group], metrics=main_metrics, sort=[{"frequency":order}], filters=filters)['results']
-                        prefix = 'Least' if order == 'asc' else 'Most'
+                        freq_group = self.dataset.aggregate(
+                            groupby=[group],
+                            metrics=main_metrics,
+                            sort=[{"frequency": order}],
+                            filters=filters,
+                        )["results"]
+                        prefix = "Least" if order == "asc" else "Most"
                         freq_str = f"{prefix} frequent {group['name']}: {freq_group[0]['frequency']} for {freq_group[0][group['name']]}"
                         self.paragraph(freq_str)
                     for metric in main_metrics:
                         for order in ["asc", "desc"]:
-                            group = self.dataset.aggregate(groupby=[group], metrics=main_metrics, sort=[{metric['name']:order}], filters=filters)['results']
-                            prefix = 'Lowest' if order == 'asc' else 'Highest'
+                            group = self.dataset.aggregate(
+                                groupby=[group],
+                                metrics=main_metrics,
+                                sort=[{metric["name"]: order}],
+                                filters=filters,
+                            )["results"]
+                            prefix = "Lowest" if order == "asc" else "Highest"
                             group_str = f"{prefix} {metric['name']} for {group['name']}: {round(group[0][metric['name']], decimals)} for {group[0][group['name']]}"
                             self.paragraph(group_str)
             elif depth > 1:
                 for group in list(itertools.combinations(main_groupby, depth)):
-                    group_name = ' & '.join([g['name'] for g in group])
-                    self.h2(f"Looking at ({group_name})")
+                    group_name_str = " & ".join([g["name"] for g in group])
+                    self.h2(f"Looking at ({group_name_str})")
 
                     for order in ["desc", "asc"]:
-                        freq_group = self.dataset.aggregate(groupby=list(group), metrics=main_metrics, sort=[{"frequency":order}], filters=filters)['results']
+                        freq_group = self.dataset.aggregate(
+                            groupby=list(group),
+                            metrics=main_metrics,
+                            sort=[{"frequency": order}],
+                            filters=filters,
+                        )["results"]
                         freq_group_name = []
                         for g in group:
-                            freq_group_name.append(freq_group[0][g['name']])
-                        freq_group_name = " & ".join(freq_group_name)
-                        prefix = 'Least' if order == 'asc' else 'Most'
-                        freq_group_str = f"{prefix} frequent {group_name}: {freq_group[0]['frequency']} for {freq_group_name}"
+                            freq_group_name.append(freq_group[0][g["name"]])
+                        freq_group_name_str = " & ".join(freq_group_name)
+                        prefix = "Least" if order == "asc" else "Most"
+                        freq_group_str = f"{prefix} frequent for {group_name_str}: {freq_group[0]['frequency']} for {freq_group_name_str}"
                         self.paragraph(freq_group_str)
                     for m in main_metrics:
                         for order in ["desc", "asc"]:
-                            group = self.dataset.aggregate(groupby=list(group), metrics=main_metrics, sort=[{m['name']:order}], filters=filters)['results']
-                            group_name = []
+                            metric_group = self.dataset.aggregate(
+                                groupby=list(group),
+                                metrics=main_metrics,
+                                sort=[{m["name"]: order}],
+                                filters=filters,
+                            )["results"]
+                            metric_group_name = []
                             for g in group:
-                                group_name.append(group[0][g['name']])
-                            group_name = " & ".join(group_name)
-                            prefix = 'Lowest' if order == 'asc' else 'Highest'
-                            group_str = f"{prefix} {m['name']} for {group_name}: {round(group[0][m['name']], decimals)} for {group_name}"
+                                metric_group_name.append(group[0][g["name"]])
+                            metric_group_name_str = " & ".join(metric_group_name)
+                            prefix = "Lowest" if order == "asc" else "Highest"
+                            group_str = f"{prefix} {m['name']} for {group_name_str}: {round(group[0][m['name']], decimals)} for {metric_group_name_str}"
                             self.paragraph(group_str)
