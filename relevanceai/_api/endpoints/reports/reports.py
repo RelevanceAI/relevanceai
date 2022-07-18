19e3fc49719084a3a5ba485bc7879c6c7bb6fc78
Jacky Koh <jacky.koh@vylar.org>
2022-07-18 16:30:47 +1000

    update

--- relevanceai/_api/endpoints/reports/reports.py
+++
@@ -1,9 +0,0 @@
-from relevanceai.client.helpers import Credentials
-from relevanceai.utils.base import _Base
-from relevanceai._api.endpoints.reports.clusters import ClusterReportClient
-
-
-class ReportsClient(_Base):
-    def __init__(self, credentials: Credentials):
-        self.clusters = ClusterReportClient(credentials)
-        super().__init__(credentials)
