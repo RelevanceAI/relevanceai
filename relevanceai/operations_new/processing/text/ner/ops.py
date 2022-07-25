438d7a30d977f5a29b2110409043cd90015931af
Jacky Wong <jacky.wong@relevance.ai>
2022-07-25 22:08:19 +1000

    add NER

--- relevanceai/operations_new/processing/text/ner/ops.py
+++ relevanceai/operations_new/processing/text/ner/ops.py
@@ -7,7 +7,14 @@ from relevanceai.operations_new.processing.text.ner.transform import ExtractNER
 
 
 class ExtractNEROps(OperationAPIBase, ExtractNER):
-    def __init__(self, credentials, fields, output_fields, **kwargs):
+    def __init__(
+        self,
+        credentials,
+        fields,
+        output_fields,
+        model_id: str = "dslim/bert-base-NER",
+        **kwargs
+    ):
         self.credentials = credentials
         self.fields = fields
         if output_fields is None:
@@ -16,3 +23,5 @@ class ExtractNEROps(OperationAPIBase, ExtractNER):
             self.output_fields = output_fields
         for k, v in kwargs.items():
             setattr(self, k, v)
+        self.model_id = model_id
+        super().__init__()
