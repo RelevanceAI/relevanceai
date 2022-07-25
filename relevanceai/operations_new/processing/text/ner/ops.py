010d7f2dcabf2b9a40997e584ce8e24e93e64f85
Jacky Wong <jacky.wong@relevance.ai>
2022-07-25 22:00:27 +1000

    fix ner

--- relevanceai/operations_new/processing/text/ner/ops.py
+++ relevanceai/operations_new/processing/text/ner/ops.py
@@ -7,4 +7,11 @@ from relevanceai.operations_new.processing.text.ner.transform import ExtractNER
 
 
 class ExtractNEROps(OperationAPIBase, ExtractNER):
-    pass
+    def __init__(self, credentials, fields, output_fields, **kwargs):
+        self.credentials = credentials
+        if output_fields is None:
+            self.output_fields = [self._generate_output_field(f) for f in fields]
+        else:
+            self.output_fields = output_fields
+        for k, v in kwargs.items():
+            setattr(self, k, v)
