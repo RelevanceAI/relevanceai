ba9a779abcce612bb4d37d88a1cf8b4a3e10d984
jacky koh <jacky@relevance.ai>
2022-08-08 14:25:09 +1000

    separate it out, fyi ordinary list doesnt work

--- relevanceai/apps/report_app/advanced_blocks/advanced_blocks.py
+++ relevanceai/apps/report_app/advanced_blocks/advanced_blocks.py
@@ -1,9 +1,10 @@
 from relevanceai.apps.report_app.advanced_blocks.pyplot import PyplotReportBlock
 from relevanceai.apps.report_app.advanced_blocks.plotly import PlotlyReportBlock
 from relevanceai.apps.report_app.advanced_blocks.altair import AltairReportBlock
+from relevanceai.apps.report_app.advanced_blocks.markdown import MarkdownBlock
 
 
-class ReportAdvancedBlocks(PyplotReportBlock, PlotlyReportBlock, AltairReportBlock):
+class ReportAdvancedBlocks(PyplotReportBlock, PlotlyReportBlock, AltairReportBlock, MarkdownBlock):
     def plot_by_method(
         self, plot, plot_method, title="", height=None, width=None, add=True
     ):
