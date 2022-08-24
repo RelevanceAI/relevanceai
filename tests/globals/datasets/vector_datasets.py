4fe6cf379fec807498918aa231133f835365bec7
jtwinrelevanceai <joseph.twin@relevance.ai>
2022-08-24 17:06:36 +1000

    fixed some tests

--- tests/globals/datasets/vector_datasets.py
+++ tests/globals/datasets/vector_datasets.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 
 from typing import Dict, List
@@ -11,5 +12,6 @@ from tests.globals.constants import generate_dataset_id
 def vector_dataset_id(test_client: Client, vector_documents: List[Dict]):
     test_dataset_id = generate_dataset_id()
     test_client._insert_documents(test_dataset_id, vector_documents)
+    time.sleep(1)
     yield test_dataset_id
     test_client.datasets.delete(test_dataset_id)
