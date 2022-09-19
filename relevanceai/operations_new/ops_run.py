5145bb4ac35d8fa5c264e0e31812913d4176e306
Jacky Wong <jacky.wong@relevance.ai>
2022-09-19 23:31:05 +1000

    update the inequality

--- relevanceai/operations_new/ops_run.py
+++ relevanceai/operations_new/ops_run.py
@@ -368,7 +368,9 @@ class PullTransformPush:
         # Check for early termination (such as no documents)
         HAS_KILL_SIGNAL: bool = False
         print("Begin transform.")
-        while self.transform_count < self.ndocs and not self.timeout_event.is_set():
+        print(self.transform_count)
+        print(self.ndocs)
+        while self.transform_count <= self.ndocs and not self.timeout_event.is_set():
             print("Inside transform loop.")
             with self.transform_batch_lock:
                 batch = self._get_transform_batch()
