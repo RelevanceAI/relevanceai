0eaf38d5be70644eb54dad9d8a4a7162a54d2c11
Jacky Koh <jackykoh@Relevance-AI-Unused.local>
2023-06-03 08:08:13 -0700

    update

--- relevanceai/steps/__init__.py
+++ relevanceai/steps/__init__.py
@@ -6,3 +6,5 @@ from relevanceai.steps.redis_vector_search import RedisSearch
 from relevanceai.steps.vectorize_and_search import VectorizeAndSearchArray
 from relevanceai.steps.execute_javascript import ExecuteJavascriptCode
 from relevanceai.steps.generate_vector_embedding import GenerateVectorEmbedding
+from relevanceai.steps.run_chain import RunChain
+from relevanceai.steps.run_step import RunStep, list_all_steps
---
+++ relevanceai/steps/run_step.py
@@ -0,0 +1,58 @@
+import requests
+from relevanceai._request import handle_response
+from relevanceai.auth import config, Auth
+from relevanceai.steps._base import StepBase
+
+
+def list_all_steps(auth: Auth = None):
+    if auth is None:
+        auth = config.auth
+    response = requests.get(
+        f"https://api-{auth.region}.stack.tryrelevance.com/latest/studios/transformations/list",
+    )
+    res = handle_response(response)
+    results_list = []
+    for s in res["transformations"]:
+        results_list.append(
+            {
+                "id": s["transformation_id"],
+                "name": s["name"],
+                "description": s["description"],
+            }
+        )
+    return results_list
+
+
+class RunStep(StepBase):
+    def __init__(self, step_id: str, step_name: str = None, *args, **kwargs):
+        self.list_of_steps = list_all_steps()
+        self.step_id = step_id
+        for step in self.list_of_steps:
+            if step["transformation_id"] == self.step_id:
+                self.step_definition = step
+        self.step_name = step["name"]
+        self._inputs = [
+            t for t in self.step_definition["input_schema"]["properties"].keys()
+        ]
+        self._required = (
+            self.step_definition["input_schema"]["required"]
+            if "required" in self.step_definition["input_schema"]
+            else []
+        )
+        self._outputs = [
+            t for t in self.step_definition["output_schema"]["properties"].keys()
+        ]
+        self.outputs = [f"steps.{self.step_name}.output.{a}" for a in self._outputs]
+        super().__init__(*args, **kwargs)
+
+    @property
+    def steps(self):
+        return [
+            {
+                "transformation": "run_chain",
+                "name": self.step_name,
+                "foreach": "",
+                "output": {output: f"{{{{ {output} }}}}" for output in self._outputs},
+                "params": {"chain_id": self.chain_id, "params": self.params},
+            }
+        ]
