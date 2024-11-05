24ba0afdb741c751eafc0c34eb50e815f34f551c
Aman Arora <aman@relevanceai.com>
2024-11-05 13:37:29 +1100

    Add ability to get conversation metadata

--- relevanceai/resources/tasks.py
+++ relevanceai/resources/tasks.py
@@ -1,7 +1,8 @@
 from .._client import RelevanceAI
 from .._resource import SyncAPIResource
 from ..types.task import Task, TriggeredTask, ScheduledActionTrigger, TaskConversation, TaskView
-from typing import Optional, List
+from ..types.knowledge import Metadata
+from typing import Optional, List, Union
 import json 
 
 class Tasks(SyncAPIResource): 
@@ -174,3 +175,11 @@ class Tasks(SyncAPIResource):
         body = {"knowledge_set": [conversation_id]}
         response = self._post(path=path, body=body)
         return response.status_code == 200
+
+    def get_metadata(
+            self, 
+            conversation_id: str
+    ) -> Union[Metadata, dict]:
+        path = f"knowledge/sets/{conversation_id}/get_metadata"
+        response = self._get(path).json()
+        return Metadata(**response['metadata']) if response.get('metadata') else {}
