4547e7146826c019905462a5b07767c4e30aef6e
Ethan Trang <ethan.trang5521@gmail.com>
2024-11-29 15:32:18 +1100

    change versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="relevanceai",
-    version="10.0.4",
+    version="10.0.5",
     url="https://relevanceai.com/",
     author="Relevance AI",
     author_email="jacky@relevanceai.com",
